7143d3536f3ce1adf44c28f2aa82ee988074a557
kompotkot <sumarokovsergei@yandex.ru>
2023-02-06 13:28:44 +0000

    Format after main merge

--- pages/whitelist/index.tsx
+++ pages/whitelist/index.tsx
@@ -1,83 +1,66 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-import {
-  Box,
-  Button,
-  Center,
-  Flex,
-  Input,
-  Text,
-  useToast,
-} from '@chakra-ui/react';
-import { useEffect, useState } from 'react';
+import { Box, Button, Center, Flex, Input, Text, useToast } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
 
-import Layout from '../../src/components/layout';
-import {
-  useSearchPublicEntity,
-  useCreatePublicEntity,
-} from '../../src/hooks/usePublicEntities';
+import Layout from '../../src/components/layout'
+import { useSearchPublicEntity, useCreatePublicEntity } from '../../src/hooks/usePublicEntities'
 
 const Whitelist = () => {
-  const toast = useToast();
-  const [searchingAddress, setSearchingAddress] = useState('');
-  const [claimedAddress, setClaimedAddress] = useState('');
+  const toast = useToast()
+  const [searchingAddress, setSearchingAddress] = useState('')
+  const [claimedAddress, setClaimedAddress] = useState('')
 
   const onSuccess = () => {
     // Handle successful request
-  };
+  }
   const onError = (error: any) => {
-    console.log(error?.message);
+    console.log(error?.message)
     toast({
       render: () => (
-        <Box
-          borderRadius='5px'
-          textAlign='center'
-          color='black'
-          p={1}
-          bg='red.600'
-        >
+        <Box borderRadius='5px' textAlign='center' color='black' p={1} bg='red.600'>
           {error?.message}
         </Box>
       ),
       isClosable: true,
-    });
-  };
+    })
+  }
 
   const {
     isLoading: isLoadingSearch,
     data: dataSearch,
     isFetching: isFetchingSearch,
     refetch: refetchSearch,
-  } = useSearchPublicEntity(onSuccess, onError, searchingAddress);
+  } = useSearchPublicEntity(onSuccess, onError, searchingAddress)
   const {
     isLoading: isLoadingCreate,
     data: dataCreate,
     isFetching: isFetchingCreate,
     refetch: refetchCreate,
-  } = useCreatePublicEntity(onSuccess, onError, searchingAddress);
+  } = useCreatePublicEntity(onSuccess, onError, searchingAddress)
 
   const handleSubmit = () => {
-    refetchSearch();
-  };
+    refetchSearch()
+  }
 
   const handleKeyDown = (e: React.KeyboardEvent) => {
     if (e.code === 'Enter') {
-      handleSubmit();
+      handleSubmit()
     }
-  };
+  }
 
   useEffect(() => {
     if (dataSearch) {
       if (dataSearch.total_results === 0) {
-        refetchCreate();
+        refetchCreate()
       }
     }
-  }, [dataSearch]);
+  }, [dataSearch])
 
   useEffect(() => {
     if (dataCreate) {
-      setClaimedAddress(dataCreate.address);
+      setClaimedAddress(dataCreate.address)
     }
-  }, [dataCreate]);
+  }, [dataCreate])
 
   return (
     <Layout home={true}>
@@ -92,31 +75,19 @@ const Whitelist = () => {
               value={searchingAddress}
               onChange={(e) => setSearchingAddress(e.target.value)}
             />
-            <Button
-              bg='gray.0'
-              fontWeight='400'
-              fontSize='18px'
-              color='#2d2d2d'
-              onClick={handleSubmit}
-            >
+            <Button bg='gray.0' fontWeight='400' fontSize='18px' color='#2d2d2d' onClick={handleSubmit}>
               Claim
             </Button>
           </Flex>
           {claimedAddress && (
-            <Flex
-              direction='column'
-              gap='20px'
-              bg='#2d2d2d'
-              borderRadius='10px'
-              p='20px'
-            >
+            <Flex direction='column' gap='20px' bg='#2d2d2d' borderRadius='10px' p='20px'>
               <Text>Claimed for {claimedAddress}</Text>
             </Flex>
           )}
         </Flex>
       </Center>
     </Layout>
-  );
-};
+  )
+}
 
-export default Whitelist;
+export default Whitelist
--- src/hooks/usePublicEntities.ts
+++ src/hooks/usePublicEntities.ts
@@ -1,60 +1,42 @@
-import { useQuery } from 'react-query';
-import axios from 'axios';
+import { useQuery } from 'react-query'
+import axios from 'axios'
 
-const ENTITY_API = process.env.NEXT_PUBLIC_ENTITY_API_URL;
-const WHITELIST_EVENT_COLLECTION_ID =
-  process.env.NEXT_PUBLIC_WHITELIST_EVENT_COLLECTION_ID;
+const ENTITY_API = process.env.NEXT_PUBLIC_ENTITY_API_URL
+const WHITELIST_EVENT_COLLECTION_ID = process.env.NEXT_PUBLIC_WHITELIST_EVENT_COLLECTION_ID
 
 const fetchSearchPublicEntity = (queryKey: any) => {
-  const searchingAddress = queryKey.queryKey[1];
-  return axios.get(
-    `${ENTITY_API}/public/collections/${WHITELIST_EVENT_COLLECTION_ID}/search?required_field=address:${searchingAddress}`,
-  );
-};
+  const searchingAddress = queryKey.queryKey[1]
+  return axios.get(`${ENTITY_API}/public/collections/${WHITELIST_EVENT_COLLECTION_ID}/search?required_field=address:${searchingAddress}`)
+}
 
-export const useSearchPublicEntity = (
-  onSuccess: any,
-  onError: any,
-  searchingAddress: string,
-) => {
-  return useQuery(
-    ['search-public-entity', searchingAddress],
-    fetchSearchPublicEntity,
-    {
-      onSuccess,
-      onError,
-      select: (data: any) => {
-        return data?.data;
-      },
-      enabled: false,
+export const useSearchPublicEntity = (onSuccess: any, onError: any, searchingAddress: string) => {
+  return useQuery(['search-public-entity', searchingAddress], fetchSearchPublicEntity, {
+    onSuccess,
+    onError,
+    select: (data: any) => {
+      return data?.data
     },
-  );
-};
+    enabled: false,
+  })
+}
 
 const fetchCreatePublicEntity = (queryKey: any) => {
-  const address = queryKey.queryKey[1];
+  const address = queryKey.queryKey[1]
   const data = {
     address: address,
     blockchain: 'polygon',
     name: 'Public claimant',
-  };
-  return axios.post(
-    `${ENTITY_API}/public/collections/${WHITELIST_EVENT_COLLECTION_ID}/entities`,
-    data,
-  );
-};
+  }
+  return axios.post(`${ENTITY_API}/public/collections/${WHITELIST_EVENT_COLLECTION_ID}/entities`, data)
+}
 
-export const useCreatePublicEntity = (
-  onSuccess: any,
-  onError: any,
-  address: string,
-) => {
+export const useCreatePublicEntity = (onSuccess: any, onError: any, address: string) => {
   return useQuery(['create-public-entity', address], fetchCreatePublicEntity, {
     onSuccess,
     onError,
     select: (data: any) => {
-      return data?.data;
+      return data?.data
     },
     enabled: false,
-  });
-};
+  })
+}
