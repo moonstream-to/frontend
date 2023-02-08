2b7656fba87b7854e77f66ff5146bd782105c005
Anton Mushnin <anton.mushnin@gmail.com>
2023-02-08 12:18:09 +0300

    edit uri

--- src/components/TerminusPoolView.tsx
+++ src/components/TerminusPoolView.tsx
@@ -2,19 +2,65 @@
 import Spinner from './Spinner/Spinner'
 import { Flex, Text } from '@chakra-ui/layout'
 import { Image } from '@chakra-ui/image'
-import { useQuery } from 'react-query'
+import { useQuery, useMutation } from 'react-query'
 const terminusAbi = require('../web3/abi/MockTerminus.json')
 const multicallABI = require('../web3/abi/Multicall2.json')
 import { MockTerminus } from '../web3/contracts/types/MockTerminus'
 import queryCacheProps from '../hooks/hookCommon'
 import PoolDetailsRow from './PoolDetailsRow'
 import { MULTICALL2_CONTRACT_ADDRESSES } from '../constants'
+import { useContext, useState } from 'react'
+import { Button, Input, useToast } from '@chakra-ui/react'
 import Web3Context from '../contexts/Web3Context/context'
-import { useContext } from 'react'
+
 
 const TerminusPoolView = ({ address, poolId, metadata }: { address: string; poolId: string; metadata: any }) => {
-  const { chainId, web3 } = useContext(Web3Context)
+  const { chainId, web3, account } = useContext(Web3Context)
+
   const headerMeta = ['name', 'description', 'image', 'attributes']
+  const [newUri, setNewUri] = useState('');
+  // const web3ctx = useContext(Web3Context)
+  const terminusFacet = new web3.eth.Contract(
+    terminusAbi
+  ) as any as MockTerminus;
+  terminusFacet.options.address = address;
+  const toast = useToast();
+  const commonProps = {
+    onSuccess: () => {
+      toast({
+        title: 'Successfully updated contract',
+        status: 'success',
+        duration: 5000,
+        isClosable: true,
+      })
+      // contractState.refetch();
+    },
+    onError: () => {
+      toast({
+        title: 'Something went wrong',
+        status: 'error',
+        duration: 5000,
+        isClosable: true,
+      })
+    },
+  };
+  
+  const setPoolURI = useMutation(
+    ({ uri, poolId }: { uri: string; poolId: string }) =>
+      terminusFacet.methods.setURI(poolId, uri).send({ from: account }),
+    { ...commonProps }
+  );
+
+  const handleNewUri = () => {
+    setPoolURI.mutate(
+      { uri: newUri, poolId: poolId },
+      {
+        onSettled: () => {
+          poolState.refetch();
+        },
+      }
+    );
+  }
 
   const poolState = useQuery(
     ['poolState', address, poolId, chainId],
@@ -136,6 +182,10 @@ const TerminusPoolView = ({ address, poolId, metadata }: { address: string; pool
                       ))}
                     </>
                   )}
+              <Flex gap='15px'>
+                <Input placeholder='new uri' value={newUri} onChange={(e) => setNewUri(e.target.value)}type='url'/>
+                <Button bg='gray.0' fontWeight='400' fontSize='18px' color='#2d2d2d' onClick={handleNewUri}>Save</Button>
+              </Flex>
                 </Flex>
               )}
           </Flex>
