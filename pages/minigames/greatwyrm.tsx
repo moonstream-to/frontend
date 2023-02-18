912fd17b5ffc41b5a54b41a15a5cac274d9ef7db
Anton Mushnin <anton.mushnin@gmail.com>
2023-02-18 23:26:08 +0300

    tablet layout

--- pages/minigames/greatwyrm.tsx
+++ pages/minigames/greatwyrm.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 
-import { Button, Center, Flex, Text } from '@chakra-ui/react'
+import { Button, Center, Flex, Spacer, Text } from '@chakra-ui/react'
 
 import Layout from '../../src/components/greatWyrm/layout'
 import { AWS_ASSETS_PATH } from '../../src/constants'
@@ -22,19 +22,23 @@ export default function Home() {
         />
       </Head>
       <Center>
-        <Flex gap='40px' p='40px 22px' w='100%' direction='column' fontSize='16px' lineHeight='20px'>
-          <ImageFallback text='illustration' h='194px' />
+        <Flex gap={['40px', '80px', '80px']} py={['40px', '80px', '80px']} px={['22px', '54px', '54px']} w='100%' direction='column' fontSize={['16px', '18px', '18px']} lineHeight={['20px', '23px', '23px']}>
           <Flex direction='column' gap='20px' textAlign='center'>
-            <Text fontWeight='700' fontSize='30px' lineHeight='100%'>Enter the world of Great Wyrm</Text>
+          <ImageFallback text='illustration' h='194px' mb='20px'/>
+            <Text fontWeight='700' fontSize={['30px', '40px', '40px']} lineHeight='100%'>Enter the world of Great Wyrm</Text>
             <Text>The only player-owned role-playing game in the world. Great Wyrm players have full control of the world we all play in.</Text>
           </Flex>
-          <Flex direction='column' gap='20px' >
+          <Flex direction={['column', 'row', 'row']} gap='20px' >
             <Button 
               color='black' 
               w='100%' 
               bg='linear-gradient(92.3deg, #EB8C6A 8.4%, #FFFFFF 126.31%)' 
               _hover={{bg: 'linear-gradient(263.61deg, #EAA88F -6.84%, #FFFFFF 128.87%)'}}
               borderRadius='30px'
+              fontSize={['16px', '20px', '20px']}
+              lineHeight='26px'
+              py='10px'
+              h={['40px', '46px', '46px']}
             >
               Create a character
             </Button>
@@ -45,56 +49,79 @@ export default function Home() {
               bg='transparent'
               onClick={() => window.open('https://discord.gg/K56VNUQGvA')}
               _hover={{fontWeight: '700'}}
+              fontSize={['16px', '20px', '20px']}
+              h={['40px', '46px', '46px']}
+
+
             >
               Join our discord
             </Button>
           </Flex>
           <Flex direction='column' p='40px 20px' gap='30px' borderRadius='30px' border='1px solid white' textAlign='center'>
-            <Text fontWeight='700' fontSize='24px' lineHeight='120%'>True Open Gaming License</Text>
+            <Text fontWeight='700' fontSize={['24px', '30px', '30px']} lineHeight='120%'>True Open Gaming License</Text>
             <Text>We value and support content creators. No corporate overlords collect taxes here. All the fees go to content creators, and to support the infrastructure behind the game.</Text>
           </Flex>
-          <ImageFallback text='UI demo or illustration' h='501px' />
-          <Flex direction='column' gap='20px' textAlign='center'>
-            <Text fontWeight='700' fontSize='30px' lineHeight='120%'>Start your adventure</Text>
-            <Text>Make a character or choose an existing character to add to the game’s lore.</Text> 
-            <Text>Choose a  path to follow.  </Text>
-            <Text>Create your own stories behind the paths you choose. Form alliances based on your choices. Convince other people to join your alliance, or trick them into choosing paths that end in despair.</Text>
-          </Flex>
-          <Button 
-              color='white' 
-              w='100%' 
-              bg='#EB8C6A' 
-              _hover={{bg: '#EAA88F'}}
-              borderRadius='30px'
-              mb='40px'
-            >
-              Make a character
+
+          <Flex direction={['column', 'row', 'row']} gap={['40px', '60px', '60px']}>
+            <ImageFallback text='UI demo or illustration' minH={['501px', '0px', '0px']} minW={['100%', '0%', '0%']} flex='1'/>
+            <Flex direction='column' gap='20px' textAlign='center' flex='1'>
+              <Text fontWeight='700' fontSize='30px' lineHeight='120%'>Start your adventure</Text>
+              <Text>Make a character or choose an existing character to add to the game’s lore.</Text> 
+              <Text>Choose a  path to follow.  </Text>
+              <Text>Create your own stories behind the paths you choose. Form alliances based on your choices. Convince other people to join your alliance, or trick them into choosing paths that end in despair.</Text>
+              <Spacer />
+            <Button 
+                color='white' 
+                w='100%' 
+                bg='#EB8C6A' 
+                _hover={{bg: '#EAA88F'}}
+                borderRadius='30px'
+                mb={['40px', '0', '0']}
+                mt='20px'
+                fontSize={['16px', '20px', '20px']}
+                h={['40px', '46px', '46px']}
+
+
+                >
+                Make a character
             </Button>
-            <Flex bg='#EB8C6A' p='30px' borderRadius='20px' gap='20px' direction='column' textAlign='center'>
+                </Flex>
+          </Flex>
+
+
+            <Flex alignItems='center' bg='#EB8C6A' p='30px' borderRadius='20px' gap='20px' direction='column' textAlign='center'>
               <Text fontSize='30px' fontWeight='700' lineHeight='100%'>Join the Great Wyrm community to start the world building and be the first to play</Text>
               <Text>The game is now in early access. You can start creating your own content, making your own characters, forming alliances, or apply to be a Game Master.</Text>  
               <Button 
                 color='black' 
-                w='100%' 
+                minW={['100%', '0', '0']} 
                 bg='white' 
                 _hover={{bg: '#EAA88F'}}
                 borderRadius='30px'
                 mt='20px'
+                fontSize={['16px', '20px', '20px']}
+                h={['40px', '46px', '46px']}
+                p={['10px', '10px 80px', '10px 80px']}
               >
                 Join
               </Button>
             </Flex>
-            <Flex bg='white' color='black' p='30px' borderRadius='20px' gap='20px' direction='column' textAlign='center'>
-              <Text>Learn more about crypto, NFT and DAOs, find links to educational resources, discuss gaming projects, and laugh at memes.   </Text>  
+            <Flex alignItems='center' bg='white' color='black' p='30px' borderRadius='20px' gap={['20px', '40px', '40px']} direction={['column', 'row', 'row']} textAlign='center'>
+              <Text textAlign={['center', 'start', 'start']}>Learn more about crypto, NFT and DAOs, find links to educational resources, discuss gaming projects, and laugh at memes.   </Text>  
               <Button 
                 color='#1A1D22' 
-                w='100%' 
+                minW={['100%', 'fit-content', 'fit-content']} 
+                p={['10px', '10px 30px', '10px 30px']}
+
                 bg='white' 
                 _hover={{bg: '#EAA88F'}}
                 borderRadius='30px'
-                mt='20px'
+                mt={['20px', '0', '0']}
                 border='2px solid #1A1D22'
                 onClick={() => window.open('https://discord.gg/K56VNUQGvA')}
+                fontSize={['16px', '20px', '20px']}
+                h={['40px', '46px', '46px']}
+
               >
                 Join our Discord
               </Button>
--- src/components/greatWyrm/ImageFallback.tsx
+++ src/components/greatWyrm/ImageFallback.tsx
@@ -2,7 +2,7 @@ import { Flex, Image, Text } from "@chakra-ui/react"
 
 const ImageFallback = ({text = '', image = '', ...props }) => {
   return (
-    <Flex borderRadius='20px' bg='#4D4D4D' color='#BFBFBF' alignItems='center' justifyContent='center' {...props} minW='100%'>
+    <Flex borderRadius='20px' bg='#4D4D4D' color='#BFBFBF' alignItems='center' justifyContent='center' minW='100%' {...props} >
       {image && <Image src={image} 
         alt=''  //TODO - size 
       />}  
