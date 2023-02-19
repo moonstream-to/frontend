4841234d7a13f5484baa6358f48fe2dbd9910fc2
Anton Mushnin <anton.mushnin@gmail.com>
2023-02-19 14:19:21 +0300

    responsive navbar

--- src/components/greatWyrm/WyrmNavbar.tsx
+++ src/components/greatWyrm/WyrmNavbar.tsx
@@ -2,7 +2,7 @@ import { Text } from "@chakra-ui/react"
 
 const WyrmNavbar = ({...props}: {[x: string]: any}) => {
   return (
-    <Text p='12px 22px' fontWeight='700' fontSize='24px' lineHeight='120%' {...props} borderBottom='1px solid white'>great wyrm</Text>
+    <Text py={{base: '12px', md: '15px'}} pl={{base: '22px', sm: '54px', md: '72px', l: '101px'}} fontWeight='700' fontSize={['24px', '24px', '30px']} lineHeight='120%' {...props} borderBottom='1px solid white'>great wyrm</Text>
   )
 }
 
