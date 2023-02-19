5f8f17be5741fd02d111ca5d5a331a3273f4a172
Anton Mushnin <anton.mushnin@gmail.com>
2023-02-19 13:45:06 +0300

    footer maxW

--- src/components/greatWyrm/WyrmFooter.tsx
+++ src/components/greatWyrm/WyrmFooter.tsx
@@ -23,26 +23,26 @@ const Footer = ({ home }: { home?: boolean }) => {
           </Flex>
         )}
         {!isMobileView && (
-          <Flex justifyContent='space-between' direction='column' gap='40px' p={['40px 22px', '40px 54px', '40px 54px']} borderTop='1px solid white'>
-            <Flex justifyContent='space-between'>
-              <Flex direction='column' justifyContent='space-between'>
-                <Text fontWeight='700' fontSize='30px'>great wyrm</Text>
-                <Flex gap='20px'>
-                  <Link href='https://moonstream.to/privacy-policy'>Privacy policy</Link>
-                  <Link href='https://moonstream.to/tos' ml='20px'>Terms of Service</Link>
+          <Flex justifyContent='space-between' alignItems={['', '', '', '', 'center', 'center']} direction='column' gap='40px' p={['40px 22px', '40px 54px', '40px 54px']} borderTop='1px solid white'>
+              <Flex justifyContent='space-between' maxW={['', '', '', '', '1238px']} minW={['', '', '', '', '1238px']} flexShrink='0'>
+                <Flex direction='column' justifyContent='space-between'>
+                  <Text fontWeight='700' fontSize='30px'>great wyrm</Text>
+                  <Flex gap='20px'>
+                    <Link href='https://moonstream.to/privacy-policy'>Privacy policy</Link>
+                    <Link href='https://moonstream.to/tos' ml='20px'>Terms of Service</Link>
+                  </Flex>
                 </Flex>
-              </Flex>
-              {!is768View && (
-                <Flex direction='column' justifyContent='end'>
-                  <Text textAlign='center' fontSize='14px'>© {new Date().getFullYear()} Moonstream.to All&nbsp;rights&nbsp;reserved</Text>
-                </Flex>
+                {!is768View && (
+                  <Flex direction='column' justifyContent='end'>
+                    <Text textAlign='center' fontSize='14px'>© {new Date().getFullYear()} Moonstream.to All&nbsp;rights&nbsp;reserved</Text>
+                  </Flex>
 
-                )}
-              <SocialButtons alignItems='start' />
-            </Flex>
-            {is768View && (
-              <Text textAlign='center' fontSize='14px'>© {new Date().getFullYear()} Moonstream.to All&nbsp;rights&nbsp;reserved</Text>
-            )}
+                  )}
+                <SocialButtons alignItems='start' />
+              </Flex>
+              {is768View && (
+                <Text textAlign='center' fontSize='14px'>© {new Date().getFullYear()} Moonstream.to All&nbsp;rights&nbsp;reserved</Text>
+              )}
           </Flex>
         )}
 
