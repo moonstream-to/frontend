90090f93bff77d651d0c27f8c92b2a512139ff71
Anton Mushnin <anton.mushnin@gmail.com>
2023-02-19 11:27:52 +0300

    responsive footer

--- src/components/greatWyrm/WyrmFooter.tsx
+++ src/components/greatWyrm/WyrmFooter.tsx
@@ -1,47 +1,53 @@
 import React from 'react'
 
-import { Text, Link, Image as ChakraImage, Flex, useMediaQuery } from '@chakra-ui/react'
+import { Text, Link, Flex, useMediaQuery } from '@chakra-ui/react'
 
-import SocialButton from '../SocialButton'
+import SocialButtons from '../SocialButtons'
 
-const AWS_ASSETS_PATH = `https://s3.amazonaws.com/static.simiotics.com/moonstream/assets`
 
 const Footer = ({ home }: { home?: boolean }) => {
   const [isMobileView] = useMediaQuery('(max-width: 767px)')
-  return (
-    <Flex direction='column' gap='40px' p='40px 18px' borderTop='1px solid white' alignItems='center'>
-      <Text fontWeight='700' fontSize='30px'>great wyrm</Text>
-      <Flex direction='column' gap='20px'>
-        <Text fontWeight='semibold' textAlign='center'>
-          Follow Us
-        </Text>
-        <Flex width='158px' justifyContent='space-between'>
-          <SocialButton label={'Discord'} href={'/discordleed'}>
-            <ChakraImage maxW='26px' src={`${AWS_ASSETS_PATH}/icons/discord-logo.png`} />
-          </SocialButton>
-          <SocialButton label={'Twitter'} href={'https://twitter.com/moonstreamto'}>
-            <ChakraImage maxW='24px' src={`${AWS_ASSETS_PATH}/icons/twitter-logo.png`} />
-          </SocialButton>
-          <SocialButton label={'Github'} href={'https://github.com/bugout-dev/moonstream'}>
-            <ChakraImage maxW='24px' src={`${AWS_ASSETS_PATH}/icons/github-logo.png`} />
-          </SocialButton>
-          <SocialButton label={'LinkedIn'} href={'https://www.linkedin.com/company/moonstream/'}>
-            <ChakraImage maxW='24px' src={`${AWS_ASSETS_PATH}/icons/linkedin-logo.png`} />
-          </SocialButton>
-        </Flex>
-      </Flex>
-      <Flex direction='column' gap='20px'  fontSize='14px'>
-        <Flex justifyContent='center'>
-                  <Link href='https://moonstream.to/privacy-policy'>Privacy policy</Link>
-                  <Link href='https://moonstream.to/tos' ml='20px'>
-                    Terms of Service
-                  </Link>
-        </Flex>
-        <Text>© {new Date().getFullYear()} Moonstream.to All&nbsp;rights&nbsp;reserved</Text>
-      </Flex>
-    </Flex>
+  const [is768View] = useMediaQuery('(max-width: 1023px)')
 
-  )
+  return (
+      <>
+        {isMobileView && (
+          <Flex justifyContent='space-between' direction='column' gap='40px' p='40px 18px' fontSize='14px' borderTop='1px solid white' alignItems='center'>
+            <Text fontWeight='700' fontSize='30px'>great wyrm</Text>
+            <SocialButtons />
+            <Flex justifyContent='center'>
+              <Link href='https://moonstream.to/privacy-policy'>Privacy policy</Link>
+              <Link href='https://moonstream.to/tos' ml='20px'>Terms of Service</Link>
+            </Flex>
+            <Text mt='-20px'>© {new Date().getFullYear()} Moonstream.to All&nbsp;rights&nbsp;reserved</Text>
+          </Flex>
+        )}
+        {!isMobileView && (
+          <Flex justifyContent='space-between' direction='column' gap='40px' p={['40px 22px', '40px 54px', '40px 54px']} borderTop='1px solid white'>
+            <Flex justifyContent='space-between'>
+              <Flex direction='column' justifyContent='space-between'>
+                <Text fontWeight='700' fontSize='30px'>great wyrm</Text>
+                <Flex gap='20px'>
+                  <Link href='https://moonstream.to/privacy-policy'>Privacy policy</Link>
+                  <Link href='https://moonstream.to/tos' ml='20px'>Terms of Service</Link>
+                </Flex>
+              </Flex>
+              {!is768View && (
+                <Flex direction='column' justifyContent='end'>
+                  <Text textAlign='center' fontSize='14px'>© {new Date().getFullYear()} Moonstream.to All&nbsp;rights&nbsp;reserved</Text>
+                </Flex>
+
+                )}
+              <SocialButtons alignItems='start' />
+            </Flex>
+            {is768View && (
+              <Text textAlign='center' fontSize='14px'>© {new Date().getFullYear()} Moonstream.to All&nbsp;rights&nbsp;reserved</Text>
+            )}
+          </Flex>
+        )}
+
+      </>
+    )
 }
 
 export default Footer
