6320291e04de2292677e25e3df040fd3698c4935
Anton Mushnin <anton.mushnin@gmail.com>
2023-02-22 14:45:11 +0300

    changing footer

--- src/components/SocialButtons.tsx
+++ src/components/SocialButtons.tsx
@@ -7,15 +7,15 @@ const SocialButtons = ({...props}) => {
 
   return (
     <Flex direction='column' gap='20px' fontSize='16px' fontWeight='700' {...props}>
-      <Text fontWeight='semibold' textAlign='center' px='13px'>
+      <Text fontWeight='semibold' textAlign='center' px={{base: '0', sm: '13px'}}>
         Follow Us
       </Text>
-      <Flex width='158px' justifyContent='start'>
-        <SocialButton label={'Discord'} href={'/discordleed'}>
-          <ChakraImage maxW='26px' src={`${AWS_ASSETS_PATH}/icons/discord-logo.png`} />
+      <Flex width='158px' justifyContent={{base: 'center', sm: 'start'}}>
+        <SocialButton label={'Discord'} href={'https://discord.com/invite/knBnttUPqH'}>
+          <ChakraImage filter='invert(1) invert(65%) sepia(81%) saturate(297%) hue-rotate(322deg) brightness(98%) contrast(96%)' maxW='26px' src={`${AWS_ASSETS_PATH}/icons/discord-logo.png`} />
         </SocialButton>
-        <SocialButton label={'Twitter'} href={'https://twitter.com/moonstreamto'}>
-          <ChakraImage maxW='24px' src={`${AWS_ASSETS_PATH}/icons/twitter-logo.png`} />
+        <SocialButton label={'Twitter'} href={'https://twitter.com/GreatWyrm_RPG'}>
+          <ChakraImage filter='invert(1) invert(65%) sepia(81%) saturate(297%) hue-rotate(322deg) brightness(98%) contrast(96%)' maxW='24px' src={`${AWS_ASSETS_PATH}/icons/twitter-logo.png`} />
         </SocialButton>
       </Flex>
     </Flex>
--- src/components/greatWyrm/WyrmFooter.tsx
+++ src/components/greatWyrm/WyrmFooter.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
-import { Text, Link, Flex, useMediaQuery } from '@chakra-ui/react'
+import { Text, Link, Flex, useMediaQuery, Image } from '@chakra-ui/react'
 
 import SocialButtons from '../SocialButtons'
+
+const wordmark = 'https://s3.amazonaws.com/static.simiotics.com/play/minigames/great-wyrm-wordmark-lg.png'
 
 
 const Footer = ({ home }: { home?: boolean }) => {
@@ -13,20 +15,20 @@ const Footer = ({ home }: { home?: boolean }) => {
       <>
         {isMobileView && (
           <Flex justifyContent='space-between' direction='column' gap='40px' p='40px 18px' fontSize='14px' borderTop='1px solid white' alignItems='center'>
-            <Text fontWeight='700' fontSize='30px'>great wyrm</Text>
+            <Image src={wordmark} alt='' w='200px'/>
             <SocialButtons />
             <Flex justifyContent='center'>
               <Link href='https://moonstream.to/privacy-policy'>Privacy policy</Link>
               <Link href='https://moonstream.to/tos' ml='20px'>Terms of Service</Link>
             </Flex>
-            <Text mt='-20px'>© {new Date().getFullYear()} Moonstream.to All&nbsp;rights&nbsp;reserved</Text>
+            <Text textAlign='center' mt='-20px'>© {new Date().getFullYear()} Moonstream.to All&nbsp;rights&nbsp;reserved</Text>
           </Flex>
         )}
         {!isMobileView && (
           <Flex justifyContent='space-between' alignItems={['', '', '', '', 'center', 'center']} direction='column' gap='40px' p={['40px 22px', '40px 54px', '40px 54px']} borderTop='1px solid white'>
               <Flex justifyContent='space-between' maxW={['', '', '', '', '1238px']} minW={['', '', '', '', '1238px']} flexShrink='0'>
                 <Flex direction='column' justifyContent='space-between'>
-                  <Text fontWeight='700' fontSize='30px'>great wyrm</Text>
+                  <Image src={wordmark} alt='' w='200px'/>
                   <Flex gap='20px'>
                     <Link href='https://moonstream.to/privacy-policy'>Privacy policy</Link>
                     <Link href='https://moonstream.to/tos' ml='20px'>Terms of Service</Link>
