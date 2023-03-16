7bead9bd5724b83235965197eb258a5c7dedc91f
Anton Mushnin <anton.mushnin@gmail.com>
2023-03-16 20:52:49 +0300

    indent

--- src/services/moonstream-engine.service.js
+++ src/services/moonstream-engine.service.js
@@ -5,110 +5,110 @@ const PLAY_API = `${API}/play`
 const ADMIN_API = `${API}/admin`
 
 export const getAdminList =
-	(terminusAddress, chainName, poolId, offset, limit, dropperAddress) => async () => {
-		return http({
-			method: "GET",
-			url: `${ADMIN_API}/drops`,
-			params: {
-				blockchain: chainName,
-				contract_address: dropperAddress,
-				terminus_address: encodeURIComponent(terminusAddress),
-				terminus_pool_id: poolId,
-				limit: limit,
-				offset: offset,
-			},
-		})
-	}
+  (terminusAddress, chainName, poolId, offset, limit, dropperAddress) => async () => {
+    return http({
+      method: "GET",
+      url: `${ADMIN_API}/drops`,
+      params: {
+        blockchain: chainName,
+        contract_address: dropperAddress,
+        terminus_address: encodeURIComponent(terminusAddress),
+        terminus_pool_id: poolId,
+        limit: limit,
+        offset: offset,
+      },
+    })
+  }
 
 export const getClaimSignature = ({ claimId, address }) => {
-	return http({
-		method: "GET",
-		url: `${PLAY_API}/claims/${claimId}`,
-		params: { address: address },
-	})
+  return http({
+    method: "GET",
+    url: `${PLAY_API}/claims/${claimId}`,
+    params: { address: address },
+  })
 }
 
 export const createDropperClaim =
-	({ dropperContractAddress }) =>
-	async ({ title, description, deadline, terminusAddress, terminusPoolId }) => {
-		const data = new FormData()
-		data.append("dropper_contract_address", dropperContractAddress)
-		data.append("title", title)
-		data.append("description", description)
-		data.append("claim_block_deadline", deadline)
-		terminusAddress && data.append("terminus_address", terminusAddress)
-		terminusPoolId && data.append("terminus_pool_id", terminusPoolId)
+  ({ dropperContractAddress }) =>
+  async ({ title, description, deadline, terminusAddress, terminusPoolId }) => {
+    const data = new FormData()
+    data.append("dropper_contract_address", dropperContractAddress)
+    data.append("title", title)
+    data.append("description", description)
+    data.append("claim_block_deadline", deadline)
+    terminusAddress && data.append("terminus_address", terminusAddress)
+    terminusPoolId && data.append("terminus_pool_id", terminusPoolId)
 
-		return http({
-			method: "POST",
-			url: `${API}/drops/claims`,
-			data: data,
-		})
-	}
+    return http({
+      method: "POST",
+      url: `${API}/drops/claims`,
+      data: data,
+    })
+  }
 
 export const getClaimants =
-	({ dropperClaimId }) =>
-	({ limit, offset }) => {
-		return http({
-			method: "GET",
-			url: `${ADMIN_API}/drops/${encodeURIComponent(dropperClaimId)}/claimants`,
-			params: {
-				offset: encodeURIComponent(offset),
-				limit: encodeURIComponent(limit),
-			},
-		}).then((response) => {
-			response.data.claimants.map((claimant) => {
-				claimant.amount = claimant.raw_amount || claimant.amount
-			})
-			return response
-		})
-	}
+  ({ dropperClaimId }) =>
+  ({ limit, offset }) => {
+    return http({
+      method: "GET",
+      url: `${ADMIN_API}/drops/${encodeURIComponent(dropperClaimId)}/claimants`,
+      params: {
+        offset: encodeURIComponent(offset),
+        limit: encodeURIComponent(limit),
+      },
+    }).then((response) => {
+      response.data.claimants.map((claimant) => {
+        claimant.amount = claimant.raw_amount || claimant.amount
+      })
+      return response
+    })
+  }
 
 export const setClaimants = ({ dropperClaimId, claimants }) => {
-	const data = { claimants: claimants }
+  const data = { claimants: claimants }
 
-	return http({
-		method: "POST",
-		url: `${ADMIN_API}/drops/${dropperClaimId}/claimants/batch`,
-		data: data,
-	})
+  return http({
+    method: "POST",
+    url: `${ADMIN_API}/drops/${dropperClaimId}/claimants/batch`,
+    data: data,
+  })
 }
 
 export const deleteClaimants =
-	({ dropperClaimId }) =>
-	({ list }) => {
-		return http({
-			method: "DELETE",
-			url: `${ADMIN_API}/drops/${dropperClaimId}/claimants`,
-			data: { claimants: list },
-		})
-	}
+  ({ dropperClaimId }) =>
+  ({ list }) => {
+    return http({
+      method: "DELETE",
+      url: `${ADMIN_API}/drops/${dropperClaimId}/claimants`,
+      data: { claimants: list },
+    })
+  }
 
 export const getTime = () => async () => {
-	return http({
-		method: "GET",
-		url: `${API}/now`,
-	})
+  return http({
+    method: "GET",
+    url: `${API}/now`,
+  })
 }
 
 export const getTerminus = (chainName) => async () => {
-	return http({
-		method: "GET",
-		url: `${PLAY_API}/terminus`,
-		params: { blockchain: chainName },
-	})
+  return http({
+    method: "GET",
+    url: `${PLAY_API}/terminus`,
+    params: { blockchain: chainName },
+  })
 }
 
 export const deactivate = ({ dropperClaimId }) => {
-	return http({
-		method: "PUT",
-		url: `${ADMIN_API}/drops/${dropperClaimId}/deactivate`,
-	})
+  return http({
+    method: "PUT",
+    url: `${ADMIN_API}/drops/${dropperClaimId}/deactivate`,
+  })
 }
 
 export const activate = ({ dropperClaimId }) => {
-	return http({
-		method: "PUT",
-		url: `${ADMIN_API}/drops/${dropperClaimId}/activate`,
-	})
+  return http({
+    method: "PUT",
+    url: `${ADMIN_API}/drops/${dropperClaimId}/activate`,
+  })
 }
--- src/web3/contracts/dropper.contract.ts
+++ src/web3/contracts/dropper.contract.ts
@@ -5,47 +5,47 @@ const dropperAbi = require("../abi/Dropper.json")
 import { MoonstreamWeb3ProviderInterface } from "../../types/Moonstream"
 
 export const claimDrop =
-	(dropperAddress: any, ctx: MoonstreamWeb3ProviderInterface) =>
-	async ({
-		message,
-		blockDeadline,
-		claimId,
-		transactionConfig,
-		amount,
-	}: {
-		message: string
-		blockDeadline: string
-		claimId: string
-		transactionConfig?: any
-		amount: string
-	}) => {
-		const dropper = new ctx.web3.eth.Contract(dropperAbi) as any as Dropper
-		dropper.options.address = dropperAddress
-		const txConfig = { ...ctx.defaultTxConfig, ...transactionConfig }
+  (dropperAddress: any, ctx: MoonstreamWeb3ProviderInterface) =>
+  async ({
+    message,
+    blockDeadline,
+    claimId,
+    transactionConfig,
+    amount,
+  }: {
+    message: string
+    blockDeadline: string
+    claimId: string
+    transactionConfig?: any
+    amount: string
+  }) => {
+    const dropper = new ctx.web3.eth.Contract(dropperAbi) as any as Dropper
+    dropper.options.address = dropperAddress
+    const txConfig = { ...ctx.defaultTxConfig, ...transactionConfig }
 
-		const response = await dropper.methods
-			.claim(claimId, blockDeadline, amount, `0x` + message)
-			// .claim("1", "123", 123)
-			.send(txConfig)
-		return response
-	}
+    const response = await dropper.methods
+      .claim(claimId, blockDeadline, amount, `0x` + message)
+      // .claim("1", "123", 123)
+      .send(txConfig)
+    return response
+  }
 
 export const getState = (address: any, ctx: any) => async () => {
-	const web3 = ctx.web3
-	const dropper = new web3.eth.Contract(dropperAbi) as any as Dropper
-	dropper.options.address = address
+  const web3 = ctx.web3
+  const dropper = new web3.eth.Contract(dropperAbi) as any as Dropper
+  dropper.options.address = address
 
-	//eslint-disable-next-line
+  //eslint-disable-next-line
   const ERC20_TYPE = await dropper.methods.ERC20_TYPE().call();
-	//eslint-disable-next-line
+  //eslint-disable-next-line
   const ERC721_TYPE = await dropper.methods.ERC721_TYPE().call();
-	//eslint-disable-next-line
+  //eslint-disable-next-line
   const ERC1155_TYPE = await dropper.methods.ERC1155_TYPE().call();
-	const numClaims = await dropper.methods.numClaims().call()
-	const owner = await dropper.methods.owner().call()
-	const paused = await dropper.methods.paused().call()
+  const numClaims = await dropper.methods.numClaims().call()
+  const owner = await dropper.methods.owner().call()
+  const paused = await dropper.methods.paused().call()
 
-	return { ERC20_TYPE, ERC721_TYPE, ERC1155_TYPE, numClaims, owner, paused }
+  return { ERC20_TYPE, ERC721_TYPE, ERC1155_TYPE, numClaims, owner, paused }
 }
 
 /*
@@ -64,49 +64,49 @@ export const getState = (address: any, ctx: any) => async () => {
     }
 */
 export const getClaim = (address: any, ctx: any) => async (claimId: string) => {
-	const web3 = ctx.web3
-	const dropper = new web3.eth.Contract(dropperAbi) as any as Dropper
-	dropper.options.address = address
+  const web3 = ctx.web3
+  const dropper = new web3.eth.Contract(dropperAbi) as any as Dropper
+  dropper.options.address = address
 
-	const claim = await dropper.methods.getClaim(claimId).call()
-	const status = await dropper.methods.getClaimStatus(claimId, ctx.account).call()
-	const claimUri = await dropper.methods.claimUri(claimId).call()
-	const signer = await dropper.methods.getSignerForClaim(claimId).call()
+  const claim = await dropper.methods.getClaim(claimId).call()
+  const status = await dropper.methods.getClaimStatus(claimId, ctx.account).call()
+  const claimUri = await dropper.methods.claimUri(claimId).call()
+  const signer = await dropper.methods.getSignerForClaim(claimId).call()
 
-	return { claim, status, claimUri, signer }
+  return { claim, status, claimUri, signer }
 }
 
 export const getSignerForClaim = (address: any, ctx: any) => async (claimId: string) => {
-	const web3 = ctx.web3
-	const dropper = new web3.eth.Contract(dropperAbi) as any as Dropper
-	dropper.options.address = address
+  const web3 = ctx.web3
+  const dropper = new web3.eth.Contract(dropperAbi) as any as Dropper
+  dropper.options.address = address
 
-	const result = await dropper.methods.getSignerForClaim(claimId).call()
+  const result = await dropper.methods.getSignerForClaim(claimId).call()
 
-	return result
+  return result
 }
 
 export const createClaim =
-	(dropperAddress: any, ctx: MoonstreamWeb3ProviderInterface) =>
-	async ({
-		rewardAddress,
-		rewardType,
-		rewardId,
-		transactionConfig,
-		amount,
-	}: {
-		rewardType: string
-		rewardAddress: string
-		rewardId: string
-		transactionConfig?: any
-		amount: string
-	}) => {
-		const dropper = new ctx.web3.eth.Contract(dropperAbi) as any as Dropper
-		dropper.options.address = dropperAddress
-		const txConfig = { ...ctx.defaultTxConfig, ...transactionConfig }
+  (dropperAddress: any, ctx: MoonstreamWeb3ProviderInterface) =>
+  async ({
+    rewardAddress,
+    rewardType,
+    rewardId,
+    transactionConfig,
+    amount,
+  }: {
+    rewardType: string
+    rewardAddress: string
+    rewardId: string
+    transactionConfig?: any
+    amount: string
+  }) => {
+    const dropper = new ctx.web3.eth.Contract(dropperAbi) as any as Dropper
+    dropper.options.address = dropperAddress
+    const txConfig = { ...ctx.defaultTxConfig, ...transactionConfig }
 
-		const response = await dropper.methods
-			.createClaim(rewardType, rewardAddress, rewardId, amount)
-			.send(txConfig)
-		return response
-	}
+    const response = await dropper.methods
+      .createClaim(rewardType, rewardAddress, rewardId, amount)
+      .send(txConfig)
+    return response
+  }
