235f7b9bb66e3c709fd269d01115ddf9c11d816f
Anton Mushnin <anton.mushnin@gmail.com>
2023-03-21 13:15:54 +0300

    remove upload from new claimant

--- src/components/NewClaimantView.tsx
+++ src/components/NewClaimantView.tsx
@@ -2,7 +2,6 @@
 import { useContext, useState } from "react"
 
 import { useMutation, useQueryClient } from "react-query"
-import Papa from "papaparse"
 import { SmallCloseIcon } from "@chakra-ui/icons"
 import { Flex, IconButton, Input, Spinner, Text, Icon, Button } from "@chakra-ui/react"
 import { AiOutlineSave } from "react-icons/ai"
@@ -10,8 +9,6 @@ import { AiOutlineSave } from "react-icons/ai"
 import Web3Context from "../contexts/Web3Context/context"
 import useMoonToast from "../hooks/useMoonToast"
 import http from "../utils/http"
-import FileUpload from "./FileUpload"
-import useDrop from "../hooks/useDrop"
 
 const NewClaimantView = ({
   claimId,
@@ -31,10 +28,6 @@ const NewClaimantView = ({
   >(undefined)
   const queryClient = useQueryClient()
   const web3ctx = useContext(Web3Context)
-  const { uploadFile } = useDrop({
-    ctx: web3ctx,
-    claimId: claimId,
-  })
 
   const onDoneAdding = (justClean = false) => {
     setNewAddress("")
@@ -111,83 +104,6 @@ const NewClaimantView = ({
     },
   )
 
-  const [isUploading, setIsUploading] = useState(false)
-  const querClient = useQueryClient()
-
-  const handleParsingError = function (error: string): void {
-    setIsUploading(false)
-    toast(error, "error", 7000)
-    throw error
-  }
-
-  const validateHeader = function (headerValue: string, column: number): string {
-    const header = headerValue.trim().toLowerCase()
-    if (column == 0 && header != "address") {
-      handleParsingError("First column header must be 'address'")
-    }
-    if (column == 1 && header != "amount") {
-      handleParsingError("Second column header must be 'amount'")
-    }
-    return header
-  }
-  let parserLineNumber = 0
-
-  const validateCellValue = function (cellValue: string, column: string): string {
-    const value = cellValue.trim()
-    if (column == "address") {
-      parserLineNumber++
-      try {
-        web3ctx.web3.utils.toChecksumAddress(value)
-      } catch (error) {
-        handleParsingError(
-          `Error parsing value '${value}' on line ${parserLineNumber}. Value in 'address' column must be a valid address.`,
-        )
-      }
-    }
-    if (column == "amount") {
-      const numVal = parseInt(value)
-      if (isNaN(numVal) || numVal < 0) {
-        handleParsingError(
-          `Error parsing value: '${value}' on line ${parserLineNumber}. Value in 'amount' column must be an integer.`,
-        )
-      }
-    }
-    return value
-  }
-
-  const onDrop = (file: any) => {
-    if (!file.length) {
-      return
-    }
-    parserLineNumber = 0
-    setIsUploading(true)
-    Papa.parse(file[0], {
-      header: true,
-      skipEmptyLines: true,
-      fastMode: true,
-      transform: validateCellValue,
-      transformHeader: validateHeader,
-      complete: (result: any) => {
-        uploadFile.mutate(
-          {
-            dropperClaimId: claimId,
-            claimants: result.data,
-          },
-          {
-            onSettled: (_, error) => {
-              setIsUploading(false)
-              if (!error) {
-                setAdding(false)
-                querClient.refetchQueries(["claimants", "claimId", claimId])
-              }
-            },
-          },
-        )
-      },
-      error: (err: Error) => handleParsingError(err.message),
-    })
-  }
-
   return (
     <>
       {!existingClaimant && (
@@ -220,7 +136,6 @@ const NewClaimantView = ({
                     aria-label="cancel"
                     icon={<Icon as={AiOutlineSave} />}
                     _hover={{ bg: "#3f3f3f" }}
-                    disabled={isUploading}
                     onClick={() =>
                       addClaimants.mutate({
                         claimants: [{ address: newAddress, amount: Number(newAmount) }],
@@ -235,24 +150,12 @@ const NewClaimantView = ({
                   aria-label="cancel"
                   icon={<SmallCloseIcon />}
                   onClick={() => onDoneAdding()}
-                  disabled={addClaimants.isLoading || isUploading}
+                  disabled={addClaimants.isLoading}
                   _hover={{ bg: "#3f3f3f" }}
                 />
               </Flex>
             </Flex>
           </Flex>
-          {!addClaimants.isLoading && (
-            <Flex position="relative">
-              <FileUpload minW="100%" isUploading={isUploading} onDrop={onDrop} />
-              <SmallCloseIcon
-                position="absolute"
-                right="10px"
-                top="10px"
-                onClick={() => setAdding(false)}
-                cursor="pointer"
-              />
-            </Flex>
-          )}
         </Flex>
       )}
       {existingClaimant && (
