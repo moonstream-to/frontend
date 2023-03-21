ed41777a2ff7c9339c6adf52bd0591d155042a51
Anton Mushnin <anton.mushnin@gmail.com>
2023-03-21 23:19:57 +0300

    turn off REQUEST_SIGNATURE check

--- src/contexts/Web3Context/index.tsx
+++ src/contexts/Web3Context/index.tsx
@@ -302,7 +302,6 @@ const Web3Provider = ({ children }: { children: JSX.Element }) => {
   }, [])
 
   React.useEffect(() => {
-    if (REQUEST_SIGNATURE == "false") return
     const token = localStorage.getItem("APP_ACCESS_TOKEN") ?? ""
     const stringToken = Buffer.from(token, "base64").toString("ascii")
     const objectToken: TokenInterface =
