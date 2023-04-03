52660f9259b5041cbd03d6342050484bf49f5677
Anton Mushnin <anton.mushnin@gmail.com>
2023-04-03 18:00:08 +0300

    adding GA script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app"
+import Script from "next/script"
 
 import { useState } from "react"
 import { QueryClientProvider, QueryClient } from "react-query"
@@ -13,14 +14,36 @@ export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(new QueryClient())
 
   return (
-    <ChakraProvider theme={theme}>
-      <QueryClientProvider client={queryClient}>
-        <Web3Context>
-          <GofpProvider>
-            <Component {...pageProps} />
-          </GofpProvider>
-        </Web3Context>
-      </QueryClientProvider>
-    </ChakraProvider>
+    <>
+      {/* <!-- Google tag (gtag.js) --> */}
+      <Script
+        strategy="afterInteractive"
+        id="ga-script-1"
+        src={"https://www.googletagmanager.com/gtag/js?id=G-TRL1FFH9MG"}
+      ></Script>
+      <Script
+        id="ga-script-2"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-TRL1FFH9MG', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <ChakraProvider theme={theme}>
+        <QueryClientProvider client={queryClient}>
+          <Web3Context>
+            <GofpProvider>
+              <Component {...pageProps} />
+            </GofpProvider>
+          </Web3Context>
+        </QueryClientProvider>
+      </ChakraProvider>
+    </>
   )
 }
