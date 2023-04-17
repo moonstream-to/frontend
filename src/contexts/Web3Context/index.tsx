a5af09e67fe77c09275cbdbb9978c1ef30c26f1a
Anton Mushnin <anton.mushnin@gmail.com>
2023-04-17 12:10:38 +0300

    adding abi scan address

--- src/contexts/Web3Context/index.tsx
+++ src/contexts/Web3Context/index.tsx
@@ -1,30 +1,30 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import router from "next/router"
+import router from "next/router";
 
-import React from "react"
-import Web3 from "web3"
+import React from "react";
+import Web3 from "web3";
 
-import Web3Context, { WALLET_STATES } from "./context"
-import { isOutdated, signAccessToken } from "../../web3/web3auth"
+import Web3Context, { WALLET_STATES } from "./context";
+import { isOutdated, signAccessToken } from "../../web3/web3auth";
 import {
   ChainInterface,
   GetMethodsAbiType,
   supportedChains,
   TokenInterface,
-} from "../../types/Moonstream"
-const REQUEST_SIGNATURE = process.env.NEXT_PUBLIC_REQUEST_SIGNATURE
+} from "../../types/Moonstream";
+const REQUEST_SIGNATURE = process.env.NEXT_PUBLIC_REQUEST_SIGNATURE;
 
 if (typeof REQUEST_SIGNATURE == "undefined") {
-  console.error("REQUEST_SIGNATURE env var is not set!")
+  console.error("REQUEST_SIGNATURE env var is not set!");
 }
 
 export const MAX_INT =
-  "115792089237316195423570985008687907853269984665640564039457584007913129639935"
+  "115792089237316195423570985008687907853269984665640564039457584007913129639935";
 
 declare global {
   interface Window {
-    ethereum: any
-    web3: Web3
+    ethereum: any;
+    web3: Web3;
   }
 }
 
@@ -36,7 +36,7 @@ const _askWalletProviderToChangeChain = async (targetChain: any, setChainId: any
           method: "wallet_switchEthereumChain",
           params: [{ chainId: `0x${targetChain?.chainId.toString(16)}` }],
         })
-        .then(() => web3?.eth.getChainId().then((id: any) => setChainId(id)))
+        .then(() => web3?.eth.getChainId().then((id: any) => setChainId(id)));
     } catch (switchError: any) {
       // This error code indicates that the chain has not been added to MetaMask.
       if (switchError.code === 4902) {
@@ -50,32 +50,34 @@ const _askWalletProviderToChangeChain = async (targetChain: any, setChainId: any
                 rpcUrls: targetChain?.rpcs,
               },
             ],
-          })
+          });
         } catch (addError) {
           // handle "add" error
         }
       } else {
-        throw switchError
+        throw switchError;
       }
       // handle other "switch" errors
     }
   } else {
-    console.error("cannot change chain when targetChain is undefined")
+    console.error("cannot change chain when targetChain is undefined");
   }
-}
+};
 export const getMethodsABI: typeof GetMethodsAbiType = (abi, name) => {
-  const index = abi.findIndex((item) => item.name === name && item.type == "function")
+  const index = abi.findIndex((item) => item.name === name && item.type == "function");
   if (index !== -1) {
-    const item = abi[index]
-    return item
-  } else throw "accesing wrong abi element"
-}
+    const item = abi[index];
+    return item;
+  } else throw "accesing wrong abi element";
+};
 
 export const chains: { [key in supportedChains]: ChainInterface } = {
   ethereum: {
     chainId: 1,
     name: "ethereum",
     rpcs: ["https://mainnet.infura.io/v3/"],
+    ABIScan:
+      "http://api.etherscan.io/api?module=contract&action=getabi&address=0xfb6916095ca1df60bb79ce92ce3ea74c37c5d359",
   },
   localhost: {
     chainId: 1337,
@@ -109,163 +111,163 @@ export const chains: { [key in supportedChains]: ChainInterface } = {
     name: "wyrm",
     rpcs: ["https://wyrm.constellationchain.xyz/http"],
   },
-}
+};
 
 export const chainByChainId: { [key: number]: string } = {
   1: "ethereum",
   1337: "localhost",
   80001: "mumbai",
   137: "polygon",
-}
+};
 
 const isKnownChain = (_chainId: number) => {
   return Object.keys(chains).some((key) => {
-    return chains[key as any as supportedChains].chainId == _chainId
-  })
-}
+    return chains[key as any as supportedChains].chainId == _chainId;
+  });
+};
 
 const Web3Provider = ({ children }: { children: JSX.Element }) => {
-  const [web3] = React.useState<Web3>(new Web3(null))
+  const [web3] = React.useState<Web3>(new Web3(null));
   const [polygonClient] = React.useState<Web3>(
     new Web3(new Web3.providers.HttpProvider("https://polygon-rpc.com")),
-  )
+  );
   const [wyrmClient] = React.useState<Web3>(
     new Web3(new Web3.providers.HttpProvider("https://wyrm.constellationchain.xyz/http")),
-  )
+  );
   const [mumbaiClient] = React.useState<Web3>(
     new Web3(new Web3.providers.HttpProvider("https://matic-mumbai.chainstacklabs.com")),
-  )
+  );
 
   const _signAccessToken = async (account: string) => {
     if (web3.currentProvider) {
-      const deadline = Math.floor(new Date().getTime() / 1000) + 24 * 60 * 60
-      const token = await signAccessToken(account, window.ethereum, deadline)
+      const deadline = Math.floor(new Date().getTime() / 1000) + 24 * 60 * 60;
+      const token = await signAccessToken(account, window.ethereum, deadline);
 
-      localStorage.setItem("APP_ACCESS_TOKEN", token)
+      localStorage.setItem("APP_ACCESS_TOKEN", token);
     }
-  }
+  };
 
-  const [targetChain, _setChain] = React.useState<ChainInterface | undefined>()
+  const [targetChain, _setChain] = React.useState<ChainInterface | undefined>();
 
-  web3.eth.transactionBlockTimeout = 100
+  web3.eth.transactionBlockTimeout = 100;
   // TODO: this flag should allow to read revert messages
   // However there seems to be abug in web3js, and setting this flag will upset metamsk badly..
   // issue: https://github.com/ChainSafe/web3.js/issues/4787
   // web3.eth.handleRevert = true;
 
-  const [buttonText, setButtonText] = React.useState(WALLET_STATES.ONBOARD)
-  const [account, setAccount] = React.useState<string>("")
-  const [chainId, setChainId] = React.useState<number>(0)
+  const [buttonText, setButtonText] = React.useState(WALLET_STATES.ONBOARD);
+  const [account, setAccount] = React.useState<string>("");
+  const [chainId, setChainId] = React.useState<number>(0);
 
   const changeChainFromWalletProvider = (_chainId: number) => {
     const chainKey = Object.keys(chains).find((_key) => {
-      const key: supportedChains = _key as any as supportedChains
-      return chains[key].chainId == _chainId
-    }) as any as supportedChains | undefined
+      const key: supportedChains = _key as any as supportedChains;
+      return chains[key].chainId == _chainId;
+    }) as any as supportedChains | undefined;
     if (chainKey) {
-      _setChain(chains[chainKey])
-      setButtonText(WALLET_STATES.CONNECTED)
+      _setChain(chains[chainKey]);
+      setButtonText(WALLET_STATES.CONNECTED);
     } else {
-      _setChain(undefined)
-      setButtonText(WALLET_STATES.UNKNOWN_CHAIN)
+      _setChain(undefined);
+      setButtonText(WALLET_STATES.UNKNOWN_CHAIN);
     }
-  }
+  };
 
   const changeChainFromUI = (chainName: supportedChains) => {
     if (window?.ethereum) {
       _askWalletProviderToChangeChain(chains[chainName], setChainId, web3).then(
         () => {
           if (chainId) {
-            _setChain(chains[chainName])
-            setButtonText(WALLET_STATES.CONNECTED)
+            _setChain(chains[chainName]);
+            setButtonText(WALLET_STATES.CONNECTED);
           }
         },
         (err: any) => {
-          console.error("changeChainFromUI:", err.message)
+          console.error("changeChainFromUI:", err.message);
         },
-      )
+      );
     }
-  }
+  };
 
   const setWeb3ProviderAsWindowEthereum = async () => {
-    let wasSetupSuccess = false
+    let wasSetupSuccess = false;
     await window.ethereum
       .request({ method: "eth_requestAccounts" })
       .then(async () => {
-        web3.setProvider(window.ethereum)
+        web3.setProvider(window.ethereum);
         web3.eth.getAccounts().then((accounts) => {
-          setAccount(accounts[0])
-        })
-        const _chainId = await web3.eth.getChainId()
-        changeChainFromWalletProvider(_chainId)
-        wasSetupSuccess = true
+          setAccount(accounts[0]);
+        });
+        const _chainId = await web3.eth.getChainId();
+        changeChainFromWalletProvider(_chainId);
+        wasSetupSuccess = true;
       })
       .catch((err: any) => {
         if (err.code === 4001) {
           // EIP-1193 userRejectedRequest error
           // If this happens, the user rejected the connection request.
-          console.log("Please connect to wallet.")
+          console.log("Please connect to wallet.");
         } else {
-          console.error(err)
+          console.error(err);
         }
-      })
+      });
 
-    return wasSetupSuccess
-  }
+    return wasSetupSuccess;
+  };
   const onConnectWalletClick = async () => {
     if (window.ethereum) {
       await setWeb3ProviderAsWindowEthereum().then((result) => {
-        if (result) console.log("wallet setup was successfull")
-        else console.warn("wallet setup failed, should go in fallback mode immediately")
-        setButtonText(result ? WALLET_STATES.CONNECTED : WALLET_STATES.CONNECT)
-      })
+        if (result) console.log("wallet setup was successfull");
+        else console.warn("wallet setup failed, should go in fallback mode immediately");
+        setButtonText(result ? WALLET_STATES.CONNECTED : WALLET_STATES.CONNECT);
+      });
     } else {
-      router.push("https://metamask.io/download/")
+      router.push("https://metamask.io/download/");
     }
-  }
+  };
 
   // When web3 has changed chainId -> represent it in state of provider
   React.useEffect(() => {
     if (web3.currentProvider) {
-      web3?.eth.getChainId().then((id) => setChainId(id))
+      web3?.eth.getChainId().then((id) => setChainId(id));
     }
-  }, [web3.currentProvider, web3?.eth])
+  }, [web3.currentProvider, web3?.eth]);
 
   //when chainId, or web3 provider, or targetChain changed -> update current account in this state
   React.useEffect(() => {
     if (targetChain?.chainId && chainId === targetChain?.chainId && web3.currentProvider) {
-      web3.eth.getAccounts().then((accounts) => setAccount(accounts[0]))
+      web3.eth.getAccounts().then((accounts) => setAccount(accounts[0]));
     }
     // eslint-disable-next-line
     //
-  }, [chainId, targetChain?.chainId, web3.currentProvider, web3.eth])
+  }, [chainId, targetChain?.chainId, web3.currentProvider, web3.eth]);
 
   const handleMetamaskChainChanged = (_chainId: string) => {
     if (chainId) {
-      setChainId(Number(_chainId))
+      setChainId(Number(_chainId));
     }
-    changeChainFromWalletProvider(Number(_chainId))
-  }
+    changeChainFromWalletProvider(Number(_chainId));
+  };
   const handleProviderAccountChanged = (_accounts: Array<string>) => {
     if (chainId === targetChain?.chainId && web3.currentProvider) {
-      setAccount(web3.utils.toChecksumAddress(_accounts[0]))
+      setAccount(web3.utils.toChecksumAddress(_accounts[0]));
     }
-  }
+  };
   // On mount
   // -> start listen to chainId changed -> update current account state in this state
   // -> listen to connected -> setup state variables
   React.useEffect(() => {
     if (chainId && targetChain?.chainId) {
-      window?.ethereum?.on("chainChanged", handleMetamaskChainChanged)
-      window?.ethereum?.on("connect", setWeb3ProviderAsWindowEthereum)
-      window?.ethereum?.on("accountsChanged", handleProviderAccountChanged)
+      window?.ethereum?.on("chainChanged", handleMetamaskChainChanged);
+      window?.ethereum?.on("connect", setWeb3ProviderAsWindowEthereum);
+      window?.ethereum?.on("accountsChanged", handleProviderAccountChanged);
     }
 
     return () => {
-      window?.ethereum?.removeListener("connect", setWeb3ProviderAsWindowEthereum)
-      window?.ethereum?.removeListener("chainChanged", handleMetamaskChainChanged)
-      window?.ethereum?.removeListener("accountsChanged", handleProviderAccountChanged)
-    }
+      window?.ethereum?.removeListener("connect", setWeb3ProviderAsWindowEthereum);
+      window?.ethereum?.removeListener("chainChanged", handleMetamaskChainChanged);
+      window?.ethereum?.removeListener("accountsChanged", handleProviderAccountChanged);
+    };
     //eslint-disable-next-line
   }, [chainId, targetChain?.chainId])
 
@@ -273,18 +275,18 @@ const Web3Provider = ({ children }: { children: JSX.Element }) => {
   React.useEffect(() => {
     if (web3.currentProvider && chainId && targetChain?.chainId && account) {
       if (isKnownChain(chainId)) {
-        setButtonText(WALLET_STATES.CONNECTED)
+        setButtonText(WALLET_STATES.CONNECTED);
       } else {
-        setButtonText(WALLET_STATES.UNKNOWN_CHAIN)
+        setButtonText(WALLET_STATES.UNKNOWN_CHAIN);
       }
     } else {
       if (!window.ethereum) {
-        setButtonText(WALLET_STATES.ONBOARD)
+        setButtonText(WALLET_STATES.ONBOARD);
       } else {
-        setButtonText(WALLET_STATES.CONNECT)
+        setButtonText(WALLET_STATES.CONNECT);
       }
     }
-  }, [web3.currentProvider, chainId, targetChain, account])
+  }, [web3.currentProvider, chainId, targetChain, account]);
 
   // onMount check if there is provided address by provider already, if yes - set it in this state and provide to web3
   // As well as try to look up for chainId in list of supported chains
@@ -293,25 +295,25 @@ const Web3Provider = ({ children }: { children: JSX.Element }) => {
       setWeb3ProviderAsWindowEthereum().then((result) => {
         if (result) {
           window?.ethereum?.request({ method: "eth_chainId" }).then((_chainId: any) => {
-            changeChainFromWalletProvider(parseInt(_chainId, 16))
-          })
-        } else console.warn("provider setup failed, should go in fallback mode immediately")
+            changeChainFromWalletProvider(parseInt(_chainId, 16));
+          });
+        } else console.warn("provider setup failed, should go in fallback mode immediately");
 
-        setButtonText(result ? WALLET_STATES.CONNECTED : WALLET_STATES.CONNECT)
-      })
+        setButtonText(result ? WALLET_STATES.CONNECTED : WALLET_STATES.CONNECT);
+      });
       //  ÷
     }
     //eslint-disable-next-line
   }, [])
 
   React.useEffect(() => {
-    if (REQUEST_SIGNATURE == "false") return
-    const token = localStorage.getItem("APP_ACCESS_TOKEN") ?? ""
-    const stringToken = Buffer.from(token, "base64").toString("ascii")
+    if (REQUEST_SIGNATURE == "false") return;
+    const token = localStorage.getItem("APP_ACCESS_TOKEN") ?? "";
+    const stringToken = Buffer.from(token, "base64").toString("ascii");
     const objectToken: TokenInterface =
       stringToken !== ""
         ? JSON.parse(`${stringToken}`)
-        : { address: null, deadline: null, signed_message: null }
+        : { address: null, deadline: null, signed_message: null };
 
     if (web3?.utils.isAddress(account)) {
       if (
@@ -319,13 +321,13 @@ const Web3Provider = ({ children }: { children: JSX.Element }) => {
         isOutdated(objectToken?.deadline) ||
         !objectToken.signed_message
       ) {
-        _signAccessToken(account)
+        _signAccessToken(account);
       }
     }
     //eslint-disable-next-line
   }, [account])
 
-  const defaultTxConfig = { from: account }
+  const defaultTxConfig = { from: account };
 
   return (
     <Web3Context.Provider
@@ -348,7 +350,7 @@ const Web3Provider = ({ children }: { children: JSX.Element }) => {
     >
       {children}
     </Web3Context.Provider>
-  )
-}
+  );
+};
 
-export default Web3Provider
+export default Web3Provider;
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,67 +1,68 @@
 /* eslint-disable @typescript-eslint/ban-types */
-import Web3 from "web3/types"
-import { AbiItem } from "web3-utils"
+import Web3 from "web3/types";
+import { AbiItem } from "web3-utils";
 
 export interface WalletStatesInterface {
-  ONBOARD: string
-  CONNECT: string
-  CONNECTED: string
-  UNKNOWN_CHAIN: string
+  ONBOARD: string;
+  CONNECT: string;
+  CONNECTED: string;
+  UNKNOWN_CHAIN: string;
 }
 
-export type supportedChains = "localhost" | "mumbai" | "polygon" | "ethereum"
+export type supportedChains = "localhost" | "mumbai" | "polygon" | "ethereum";
 
 export interface ChainInterface {
-  chainId: number
-  name: supportedChains
-  rpcs: Array<string>
+  chainId: number;
+  name: supportedChains;
+  rpcs: Array<string>;
+  ABIScan?: string;
 }
 
-export declare function GetMethodsAbiType<T>(abi: AbiItem[], name: keyof T): AbiItem
+export declare function GetMethodsAbiType<T>(abi: AbiItem[], name: keyof T): AbiItem;
 
 export interface TokenInterface {
-  address: string
-  deadline: number
-  signed_message: string
+  address: string;
+  deadline: number;
+  signed_message: string;
 }
 
-declare function ChangeChain(chainName: supportedChains): void
+declare function ChangeChain(chainName: supportedChains): void;
 export interface MoonstreamWeb3ProviderInterface {
-  web3: Web3
-  polygonClient: Web3
-  wyrmClient: Web3
-  mumbaiClient: Web3
-  onConnectWalletClick: Function
-  buttonText: string
-  WALLET_STATES: WalletStatesInterface
-  account: string
-  chainId: number
-  defaultTxConfig: Object
-  signAccessToken: Function
-  getMethodsABI: typeof GetMethodsAbiType
-  changeChain: typeof ChangeChain
-  targetChain: ChainInterface | undefined
+  web3: Web3;
+  polygonClient: Web3;
+  wyrmClient: Web3;
+  mumbaiClient: Web3;
+  onConnectWalletClick: Function;
+  buttonText: string;
+  WALLET_STATES: WalletStatesInterface;
+  account: string;
+  chainId: number;
+  defaultTxConfig: Object;
+  signAccessToken: Function;
+  getMethodsABI: typeof GetMethodsAbiType;
+  changeChain: typeof ChangeChain;
+  targetChain: ChainInterface | undefined;
 }
 
 export interface UpdateClaim {
-  claim_block_deadline?: string
-  claim_id?: string
-  description?: string
-  dropper_claim_id?: string
-  dropper_contract_id?: string
-  terminus_address?: string
-  terminus_pool_id?: string
-  title?: string
+  claim_block_deadline?: string;
+  claim_id?: string;
+  description?: string;
+  dropper_claim_id?: string;
+  dropper_contract_id?: string;
+  terminus_address?: string;
+  terminus_pool_id?: string;
+  title?: string;
 }
 
 export interface ClaimInterface {
-  active: boolean
-  claim_block_deadline: number
-  drop_number: number
-  description: string
-  dropper_contract_address: string
-  id: string
-  terminus_address: string
-  terminus_pool_id: number
-  title: string
+  active: boolean;
+  claim_block_deadline: number;
+  drop_number: number;
+  description: string;
+  dropper_contract_address: string;
+  id: string;
+  terminus_address: string;
+  terminus_pool_id: number;
+  title: string;
 }
