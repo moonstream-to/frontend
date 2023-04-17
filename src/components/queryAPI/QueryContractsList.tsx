835570c4bbee6afa4bc4db912337076c014211f4
Anton Mushnin <anton.mushnin@gmail.com>
2023-04-17 12:25:57 +0300

    add subscription types to context

--- src/components/queryAPI/QueryContractsList.tsx
+++ src/components/queryAPI/QueryContractsList.tsx
@@ -1,43 +1,47 @@
-import { Flex } from "@chakra-ui/react"
-import { useQuery } from "react-query"
-import queryCacheProps from "../../hooks/hookCommon"
-import useMoonToast from "../../hooks/useMoonToast"
-import { SubscriptionsService } from "../../services"
-import QueryContractsListItem from "./QueryContractsListItem"
+import { Flex } from "@chakra-ui/react";
+import { useQuery } from "react-query";
+import useQueryAPI from "../../contexts/QueryAPIContext";
+import queryCacheProps from "../../hooks/hookCommon";
+import useMoonToast from "../../hooks/useMoonToast";
+import { SubscriptionsService } from "../../services";
+import QueryContractsListItem from "./QueryContractsListItem";
 
 function compare(a: { created_at: string }, b: { created_at: string }) {
   if (a.created_at > b.created_at) {
-    return -1
+    return -1;
   }
   if (a.created_at < b.created_at) {
-    return 1
+    return 1;
   }
-  return 0
+  return 0;
 }
 
 const QueryContractsList = () => {
-  const toast = useMoonToast()
+  const { setTypes } = useQueryAPI();
+  const toast = useMoonToast();
   const getSubscriptions = async () => {
-    const response = await SubscriptionsService.getSubscriptions()
-    const types = await SubscriptionsService.getTypes()
-    return response.data
-  }
+    const response = await SubscriptionsService.getSubscriptions();
+    return response.data;
+  };
 
   const subscriptions = useQuery(["subscriptions"], getSubscriptions, {
     ...queryCacheProps,
     onError: (error) => {
-      toast(error, "error")
+      toast(error, "error");
     },
-  })
+  });
 
   const getTypes = async () => {
-    const response = await SubscriptionsService.getTypes()
-    return response.data
-  }
+    const response = await SubscriptionsService.getTypes();
+    return response.data;
+  };
 
   const types = useQuery(["subscription_types"], getTypes, {
     ...queryCacheProps,
-  })
+    onSuccess(data: any) {
+      setTypes(data.subscription_types);
+    },
+  });
 
   return (
     <>
@@ -56,6 +60,6 @@ const QueryContractsList = () => {
         </Flex>
       )}
     </>
-  )
-}
-export default QueryContractsList
+  );
+};
+export default QueryContractsList;
