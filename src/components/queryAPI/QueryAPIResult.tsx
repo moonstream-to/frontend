0146f9571a1eb497aa1d6db8298d10a0338f9015
Anton Mushnin <anton.mushnin@gmail.com>
2023-04-20 13:12:35 +0300

    query result

--- src/components/queryAPI/QueryAPIResult.tsx
+++ src/components/queryAPI/QueryAPIResult.tsx
@@ -1,15 +1,39 @@
 import { Button, Flex, Text } from "@chakra-ui/react";
+import dynamic from "next/dynamic";
 
-const QueryAPIResult = ({ result }: { result: string }) => {
+const MyJsonComponent = dynamic(() => import("../JSONEdit2"), { ssr: false });
+
+function download(filename: string, text: string) {
+  const element = document.createElement("a");
+  element.setAttribute("href", "data:text/plain;charset=utf-8," + encodeURIComponent(text));
+  element.setAttribute("download", filename);
+
+  element.style.display = "none";
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
+
+const QueryAPIResult = ({ result, filename }: { result: string; filename: string }) => {
   return (
-    <Flex direction="column" gap="20px" bg="#232323" border="1px solid #4d4d4d" borderRadius="10px">
+    <Flex
+      direction="column"
+      gap="20px"
+      p="20px"
+      bg="#232323"
+      border="1px solid #4d4d4d"
+      borderRadius="10px"
+    >
       <Flex justifyContent="space-between" alignItems="center" fontSize="16px">
         <Text fontWeight="700">JSON</Text>
-        <Button variant="transparent">
+        <Button variant="transparent" onClick={() => download(filename, result)}>
           <Text fontWeight="400">Download</Text>
         </Button>
       </Flex>
-      <Text
+      <MyJsonComponent json={result} readOnly={true} />
+      {/* <Text
         fontSize="14px"
         bg="#2d2d2d"
         border="1px solid #353535"
@@ -17,7 +41,7 @@ const QueryAPIResult = ({ result }: { result: string }) => {
         overflow="auto"
       >
         {result}
-      </Text>
+      </Text> */}
     </Flex>
   );
 };
