4ea48ee177ceea0a78e7b1d0eb8b5fe7d6ed5aa1
Anton Mushnin <anton.mushnin@gmail.com>
2023-04-25 16:34:07 +0300

    new value update logic

--- src/components/TimestampInput2.tsx
+++ src/components/TimestampInput2.tsx
@@ -8,7 +8,6 @@ const TimestampInput2 = ({
   timestamp: string;
   setTimestamp: (arg0: string) => void;
 }) => {
-  const [value, setValue] = useState(0);
   const [year, setYear] = useState("");
   const [month, setMonth] = useState("");
   const [day, setDay] = useState("");
@@ -35,27 +34,32 @@ const TimestampInput2 = ({
       date.setSeconds(0);
       date.setMilliseconds(0);
     }
+    setComponents(String(date.getTime() / 1000));
+  }, []);
+
+  useEffect(() => {
+    const newTimestamp = String(
+      Date.UTC(
+        Number(year),
+        Number(month) - 1,
+        Number(day),
+        Number(hour),
+        Number(min),
+        Number(sec),
+      ) / 1000,
+    );
+    setTimestamp(newTimestamp);
+  }, [year, month, day, hour, min, sec]);
+
+  const setComponents = (timestampString: string) => {
+    const date = new Date(Number(timestampString) * 1000);
     setYear(String(date.getUTCFullYear()));
     setMonth(String(date.getUTCMonth() + 1));
     setDay(String(date.getUTCDate()));
     setHour(String(date.getUTCHours()));
     setMin(String(date.getUTCMinutes()));
     setSec(String(date.getUTCSeconds()));
-  }, [timestamp]);
-
-  useEffect(() => {
-    const dateComponents = components.map((component) => Number(component.value));
-    setValue(
-      Date.UTC(
-        dateComponents[0],
-        dateComponents[1] - 1,
-        dateComponents[2],
-        dateComponents[3],
-        dateComponents[4],
-        dateComponents[5],
-      ) / 1000,
-    );
-  }, [year, month, day, hour, min, sec]);
+  };
 
   return (
     <Flex gap="10px" alignItems="end" fontSize="14px" w="100%">
@@ -63,9 +67,12 @@ const TimestampInput2 = ({
         minW="14ch"
         maxW="14ch"
         fontSize="14px"
-        value={value}
+        value={timestamp}
         p="10px"
-        onChange={(e) => setTimestamp(e.target.value)}
+        onChange={(e) => {
+          setTimestamp(e.target.value);
+          setComponents(e.target.value);
+        }}
         border="1px solid #4D4D4D"
       />
       {components.map((component) => (
