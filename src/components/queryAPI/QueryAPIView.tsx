64a825bd5b16c58bf8acbb951a9aa3e442297499
Anton Mushnin <anton.mushnin@gmail.com>
2023-04-25 08:08:03 +0300

    handling user change

--- src/components/queryAPI/QueryAPIView.tsx
+++ src/components/queryAPI/QueryAPIView.tsx
@@ -1,20 +1,34 @@
-import { Center, Flex } from "@chakra-ui/react";
+import { Center, Flex, Text } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { useQueryClient } from "react-query";
 import useQueryAPI from "../../contexts/QueryAPIContext";
+import useUser from "../../contexts/UserContext";
 import QueryAPINewContractView from "./QueryAPINewContractView";
 import QueryAPIQueryView from "./QueryAPIQueryView";
 import QueryContractView from "./QueryContractView";
 import QueryListView from "./QueryListView";
 
 const QueryAPIView = () => {
-  const { isCreatingContract, isShowContracts } = useQueryAPI();
+  const { isCreatingContract, isShowContracts, reset } = useQueryAPI();
+  const { user } = useUser();
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    queryClient.invalidateQueries("");
+    reset();
+  }, [user]);
+
   return (
     <Center>
-      <Flex gap="30px" py="30px" px="7%" maxH="760px" minH="760px" maxW="1600px" minW="1600px">
-        <QueryListView />
-        {isShowContracts &&
-          (!isCreatingContract ? <QueryContractView /> : <QueryAPINewContractView />)}
-        {!isShowContracts && <QueryAPIQueryView />}
-      </Flex>
+      {user && (
+        <Flex gap="30px" py="30px" px="7%" maxH="760px" minH="760px" maxW="1600px" minW="1400px">
+          <QueryListView />
+          {isShowContracts &&
+            (!isCreatingContract ? <QueryContractView /> : <QueryAPINewContractView />)}
+          {!isShowContracts && <QueryAPIQueryView />}
+        </Flex>
+      )}
+      {!user && <Text mt="80px">You should log in to use this page</Text>}
     </Center>
   );
 };
