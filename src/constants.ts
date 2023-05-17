789eeecff06658d5a3c27587cf8bda534e4d12cd
Anton Mushnin <anton.mushnin@gmail.com>
2023-05-17 16:03:05 +0300

    team page

--- src/constants.ts
+++ src/constants.ts
@@ -99,4 +99,5 @@ export const MAX_INT =
   "115792089237316195423570985008687907853269984665640564039457584007913129639935";
 
 export const AWS_ASSETS_PATH = `https://s3.amazonaws.com/static.simiotics.com/moonstream/assets`;
+export const AWS_STATIC_ASSETS_PATH = "https://static.simiotics.com/moonstream/assets";
 export const PORTAL_PATH = "https://portal.moonstream.to";
