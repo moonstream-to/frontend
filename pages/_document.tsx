24c52ffb650e15b83b84975d9a70ff3c86cdb476
kompotkot <sumarokovsergei@yandex.ru>
2023-05-30 09:40:22 +0000

    Robots in document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta name="robots" content={process.env.NEXT_PUBLIC_BUILD_TARGET == "alpha" ? "noindex" : "all"} />
         <link href="https://fonts.googleapis.com/css?family=Space Grotesk" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css?family=Lora" rel="stylesheet" />
       </Head>
