cd327b51c90fbe2fc4eacafd65711f289624ad90
Anton Mushnin <anton.mushnin@gmail.com>
2023-07-05 11:53:41 +0300

    adding search box for JSON editor

--- portal/src/components/JSONEdit2.tsx
+++ portal/src/components/JSONEdit2.tsx
@@ -3,6 +3,7 @@ import AceEditor from "react-ace";
 
 import "ace-builds/src-noconflict/mode-json";
 import "ace-builds/src-noconflict/theme-twilight";
+import "brace/ext/searchbox";
 
 const MyJsonComponent = ({
   json,
