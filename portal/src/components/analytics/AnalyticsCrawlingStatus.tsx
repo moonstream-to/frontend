fe566e2172158ad37404a7f3c9f64f9647d72483
Anton Mushnin <anton.mushnin@gmail.com>
2023-07-10 13:26:19 +0300

    showing status message flow

--- portal/src/components/analytics/AnalyticsCrawlingStatus.tsx
+++ portal/src/components/analytics/AnalyticsCrawlingStatus.tsx
@@ -9,11 +9,11 @@ interface Status {
 }
 
 const AnalyticsCrawlingStatus = ({ address }: { address: any }) => {
-  const [statusMessage, setStatusMessage] = useState("");
   const [eventsNumber, setEventsNumber] = useState(0);
   const [eventsLoaded, setEventsLoaded] = useState(0);
   const [methodsNumber, setMethodsNumber] = useState(0);
   const [methodsLoaded, setMethodsLoaded] = useState(0);
+  const [loaded, setLoaded] = useState(true);
 
   const API = process.env.NEXT_PUBLIC_MOONSTREAM_API_URL;
 
@@ -22,6 +22,7 @@ const AnalyticsCrawlingStatus = ({ address }: { address: any }) => {
     setEventsNumber(0);
     setMethodsLoaded(0);
     setMethodsNumber(0);
+    setLoaded(true);
   }, [address]);
 
   const jobs = useQuery(
@@ -40,71 +41,55 @@ const AnalyticsCrawlingStatus = ({ address }: { address: any }) => {
             !job.tags.some((t: string) => t.includes("historical_crawl_status")),
         };
       });
-      console.log(statuses);
-      console.log(res);
+      setLoaded(!statuses.some((s: Status) => !s.finished));
       setEventsNumber(statuses.filter((s: Status) => s.type === "event").length);
       setEventsLoaded(statuses.filter((s: Status) => s.type === "event" && s.finished).length);
-      // setEventsLoaded(0);
       setMethodsNumber(statuses.filter((s: Status) => s.type === "function").length);
       setMethodsLoaded(statuses.filter((s: Status) => s.type === "function" && s.finished).length);
-      setStatusMessage(
-        `events: ${statuses.filter((s: Status) => s.type === "event" && s.finished).length} from ${
-          statuses.filter((s: Status) => s.type === "event").length
-        }\nfunctions: ${
-          statuses.filter((s: Status) => s.type === "function" && s.finished).length
-        } from ${statuses.filter((s: Status) => s.type === "function").length}`,
-      );
     },
     {
       enabled: !!address.id && address.type === "smartcontract",
     },
   );
 
-  if (eventsNumber === eventsLoaded && methodsNumber === methodsLoaded) return <></>;
-
   return (
-    <Flex
-      border="1px solid #F8D672"
-      borderRadius="10px"
-      direction="column"
-      w="100%"
-      alignItems="start"
-      p="15px"
-      gap="15px"
-    >
-      {eventsNumber !== eventsLoaded && methodsNumber !== methodsLoaded && (
-        <>
-          <Flex gap="10px">
-            <Icon as={AiOutlineCloudServer} h="20px" w="20px" />
-            <Text fontWeight="700"> Historical data is loading...</Text>
-          </Flex>
-          <Text>
-            We are gathering all data related to your contract, watch data will be ready here.
-          </Text>
-        </>
+    <>
+      {!loaded && !jobs.isLoading && (
+        <Flex
+          border="1px solid #F8D672"
+          borderRadius="10px"
+          direction="column"
+          w="100%"
+          alignItems="start"
+          p="15px"
+          gap="15px"
+        >
+          {eventsNumber !== eventsLoaded && methodsNumber !== methodsLoaded && (
+            <>
+              <Flex gap="10px">
+                <Icon as={AiOutlineCloudServer} h="20px" w="20px" />
+                <Text fontWeight="700"> Historical data is loading...</Text>
+              </Flex>
+              <Text>
+                We are gathering all data related to your contract, watch data will be ready here.
+              </Text>
+            </>
+          )}
+          {eventsNumber === eventsLoaded && methodsNumber !== methodsLoaded && (
+            <>
+              <Flex gap="10px">
+                <Icon as={AiOutlineCloudServer} h="20px" w="20px" />
+                <Text fontWeight="700"> Methods are loading...</Text>
+              </Flex>
+              <Text fontSize="14px">
+                Events are loaded, you can already use events analytics. Loaded {methodsLoaded}{" "}
+                methods from {methodsNumber}
+              </Text>
+            </>
+          )}
+        </Flex>
       )}
-      {eventsNumber === eventsLoaded && methodsNumber !== methodsLoaded && (
-        <>
-          <Flex gap="10px">
-            <Icon as={AiOutlineCloudServer} h="20px" w="20px" />
-            <Text fontWeight="700"> Methods are loading...</Text>
-          </Flex>
-          <Text fontSize="14px">
-            Events are loaded, you can already use events analytics. Loaded {methodsLoaded} methods
-            from {methodsNumber}
-          </Text>
-        </>
-      )}
-
-      {/* <Text>
-        {`${eventsNumber === eventsLoaded ? "Events are loaded" : "Events are loading..."}`}
-      </Text>
-      <Text>
-        {methodsNumber === methodsLoaded
-          ? "Methods are loaded"
-          : `Methods are loading...    (${methodsLoaded}/${methodsNumber})`}
-      </Text> */}
-    </Flex>
+    </>
   );
 };
 
