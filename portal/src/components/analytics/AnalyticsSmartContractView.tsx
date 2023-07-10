eab7cf2097d1b4149386d7b6ba822dc3fbf528b0
Anton Mushnin <anton.mushnin@gmail.com>
2023-07-10 11:37:51 +0300

    using supported_interface

--- portal/src/components/analytics/AnalyticsSmartContractView.tsx
+++ portal/src/components/analytics/AnalyticsSmartContractView.tsx
@@ -3,18 +3,23 @@ import { Flex, IconButton, Input, Link, Spacer, Spinner, Text } from "@chakra-ui
 import { useEffect, useState } from "react";
 import { AiOutlineCheck, AiOutlineClose } from "react-icons/ai";
 import { useMutation, useQuery, useQueryClient } from "react-query";
+import useAnalytics from "../../contexts/AnalyticsContext";
 import queryCacheProps from "../../hooks/hookCommon";
 import useMoonToast from "../../hooks/useMoonToast";
 import { SubscriptionsService } from "../../services";
 import http from "../../utils/httpMoonstream";
 import AnalyticsAddressTags from "./AnalyticsAddressTags";
 import AnalyticsChainSelector from "./AnalyticsChainSelector";
+import AnalyticsCrawlingStatus from "./AnalyticsCrawlingStatus";
 import AnalyticsEOADetails from "./AnalyticsEOADetails";
 import AnalyticsQueryView from "./AnalyticsQueryView";
 import AnalyticsSmartContractDetails from "./AnalyticsSmartContractDetails";
 import AnalyticsSmartContractQueries, { QueryInterface } from "./AnalyticsSmartContractQueries";
 
 const AnalyticsSmartContractView = ({ address }: { address: any }) => {
+  const API = process.env.NEXT_PUBLIC_MOONSTREAM_API_URL;
+  const toast = useMoonToast();
+
   const [selectedIdx, setSelectedIdx] = useState(-1);
 
   const [queries, setQueries] = useState<QueryInterface[]>([]);
@@ -22,8 +27,11 @@ const AnalyticsSmartContractView = ({ address }: { address: any }) => {
   const [newTitle, setNewTitle] = useState("");
   const [isEditingTitle, setIsEditingTitle] = useState(false);
 
+  const { templates } = useAnalytics();
+
   useEffect(() => {
     setSelectedIdx(-1);
+    setQueries([]);
   }, [address]);
 
   const handleAddTag = (newTag: string) => {
@@ -37,62 +45,128 @@ const AnalyticsSmartContractView = ({ address }: { address: any }) => {
   const chainName =
     address?.type === "eoa" ? eoaChain : address?.subscription_type_id.split("_")[0];
 
-  const templates = useQuery(
-    ["queryTemplates", address],
-    () => {
-      return http({
-        method: "GET",
-        url: `${API}/queries/templates`,
-      }).then((res) => {
-        const EOAQueries = res.data.interfaces?.EOA;
-        const queries = res.data.queries;
-        if (address.type === "smartcontract") {
-          return queries.filter(
-            (query: any) => !EOAQueries.some((q: any) => q.context_url === query.context_url),
-          );
-        } else {
-          return EOAQueries;
+  // const templates = useQuery(
+  //   ["queryTemplates", address],
+  //   () => {
+  //     return http({
+  //       method: "GET",
+  //       url: `${API}/queries/templates`,
+  //     }).then((res) => {
+  //       console.log(res.data);
+  //       const EOAQueries = res.data.interfaces?.EOA;
+  //       const queries = res.data.queries;
+  //       if (address.type === "smartcontract") {
+  //         return queries.filter(
+  //           (query: any) => !EOAQueries.some((q: any) => q.context_url === query.context_url),
+  //         );
+  //       } else {
+  //         return EOAQueries;
+  //       }
+  //     });
+  //   },
+  //   {
+  //     ...queryCacheProps,
+  //     onError: (error: Error) => {
+  //       console.log(error);
+  //     },
+  //   },
+  // );
+
+  // const supportredInterfaces = useQuery(["supportedInterfaces", address], () => {
+  //   return http({
+  //     method: "GET",
+  //     url: `${API}/subscriptions/supported_interfaces`,
+  //     params: {
+  //       address: address.address,
+  //       blockchain: chainName,
+  //     },
+  //   })
+  //     .then((res) => console.log(res))
+  //     .catch((e) => console.log(e));
+  // });
+
+  function removeDuplicatesByContextURL(array: QueryInterface[]) {
+    const uniqueArray = [];
+    const seenContextIds = new Set();
+
+    for (const obj of array) {
+      if (!seenContextIds.has(obj.context_url)) {
+        uniqueArray.push(obj);
+        seenContextIds.add(obj.context_url);
+      }
+    }
+
+    return uniqueArray;
+  }
+
+  const supportedQueries = useQuery(
+    ["supportedQueries", address],
+    async () => {
+      if (address.type === "smartcontract") {
+        const supportedInterfaces = await http({
+          method: "GET",
+          url: `${API}/subscriptions/supported_interfaces`,
+          params: {
+            address: address.address,
+            blockchain: chainName,
+          },
+        }).then((res) => res.data.interfaces);
+        const selectors: string[] = ["any"];
+        Object.keys(supportedInterfaces).forEach((key) =>
+          selectors.push(supportedInterfaces[key].selector),
+        );
+        const queries: QueryInterface[] = [];
+        if (Object.keys(supportedInterfaces).length === 0) {
+          Object.keys(templates.data)
+            .filter((key: string) => key !== "EOA")
+            .forEach((key: string) => selectors.push(key));
         }
-      });
+        Object.keys(templates.data)
+          .filter((selector) => selectors.includes(selector))
+          .forEach((key) => templates.data[key].forEach((q: QueryInterface) => queries.push(q)));
+        return queries;
+      } else {
+        return templates.data["EOA"];
+      }
     },
     {
-      ...queryCacheProps,
-      onError: (error: Error) => {
-        console.log(error);
+      enabled: !!templates.data,
+      onSuccess: (data: QueryInterface[]) => {
+        setQueries(removeDuplicatesByContextURL(data));
       },
     },
   );
 
-  const API = process.env.NEXT_PUBLIC_MOONSTREAM_API_URL;
-  const toast = useMoonToast();
-  const getQueries = () => {
-    return http({
-      method: "GET",
-      url: `${API}/queries/list`,
-    }).then((res) => {
-      return res.data.map((q: { name: string }) => {
-        return { title: q.name.split("_").join(" "), context_url: q.name };
-      });
-    });
-  };
+  // const API = process.env.NEXT_PUBLIC_MOONSTREAM_API_URL;
+  // const toast = useMoonToast();
+  // const getQueries = () => {
+  //   return http({
+  //     method: "GET",
+  //     url: `${API}/queries/list`,
+  //   }).then((res) => {
+  //     return res.data.map((q: { name: string }) => {
+  //       return { title: q.name.split("_").join(" "), context_url: q.name };
+  //     });
+  //   });
+  // };
 
-  const userQueries = useQuery(["queries", address], getQueries, {
-    ...queryCacheProps,
-    onError: (error) => {
-      toast(error.message, "error");
-    },
-    enabled: false && address.type === "smartcontract",
-  });
+  // const userQueries = useQuery(["queries", address], getQueries, {
+  //   ...queryCacheProps,
+  //   onError: (error) => {
+  //     toast(error.message, "error");
+  //   },
+  //   enabled: false && address.type === "smartcontract",
+  // });
 
-  useEffect(() => {
-    setSelectedIdx(-1);
-  }, [address.address]);
+  // useEffect(() => {
+  //   setSelectedIdx(-1);
+  // }, [address.address]);
 
-  useEffect(() => {
-    const templatesArray = templates.data ? templates.data : [];
-    const userQueriesArray = userQueries.data ? userQueries.data : [];
-    setQueries([...templatesArray, ...userQueriesArray]);
-  }, [templates.data, userQueries.data]);
+  // useEffect(() => {
+  //   const templatesArray = templates.data ? templates.data : [];
+  //   const userQueriesArray = userQueries.data ? userQueries.data : [];
+  //   setQueries([...templatesArray, ...userQueriesArray]);
+  // }, [templates.data, userQueries.data]);
 
   const queryClient = useQueryClient();
   const updateSubscription = useMutation(SubscriptionsService.modifySubscription(), {
@@ -192,7 +266,7 @@ const AnalyticsSmartContractView = ({ address }: { address: any }) => {
         )}
         <Flex justifyContent="space-between" alignItems="center" gap="20px">
           <Text variant="title2">Analytics</Text>
-          {(userQueries.isLoading || templates.isLoading) && <Spinner size="sm" />}
+          {(supportedQueries.isFetching || templates.isLoading) && <Spinner size="sm" />}
           <Spacer />
           <Link isExternal href="https://discord.gg/K56VNUQGvA" _hover={{ textDecoration: "none" }}>
             <Text my="auto" color="#F88F78" fontSize="14px" cursor="pointer">
@@ -200,6 +274,7 @@ const AnalyticsSmartContractView = ({ address }: { address: any }) => {
             </Text>
           </Link>
         </Flex>
+        {address.type !== "eoa" && <AnalyticsCrawlingStatus address={address} />}
         {address.type === "eoa" && (
           <AnalyticsChainSelector selectedChain={eoaChain} setSelectedChain={setEoaChain} />
         )}
