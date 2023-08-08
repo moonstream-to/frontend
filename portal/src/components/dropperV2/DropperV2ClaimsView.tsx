24684ae4b11136c24f4aa8e4a9ce97a36387a6bd
Anton Mushnin <anton.mushnin@gmail.com>
2023-08-08 13:56:58 +0300

    claimant search

--- portal/src/components/dropperV2/DropperV2ClaimsView.tsx
+++ portal/src/components/dropperV2/DropperV2ClaimsView.tsx
@@ -2,13 +2,16 @@ import { SearchIcon, SmallCloseIcon } from "@chakra-ui/icons";
 import {
   Button,
   Flex,
+  Grid,
+  GridItem,
   IconButton,
   Input,
   InputGroup,
   InputRightElement,
+  Spinner,
   Text,
 } from "@chakra-ui/react";
-import { useContext, useState } from "react";
+import React, { useContext, useState } from "react";
 import { useMutation, useQuery } from "react-query";
 import Web3Context from "../../contexts/Web3Context/context";
 import useMoonToast from "../../hooks/useMoonToast";
@@ -25,20 +28,7 @@ const DropperV2ClaimsView = ({
   isContractRegistered: boolean;
 }) => {
   const [searchAddress, setSearchAddress] = useState("");
-
-  // const getContracts = () => {
-  //   return http({
-  //     method: "GET",
-  //     url: `https://engineapi.moonstream.to/metatx/contracts`,
-  //     // params: {
-  //     //   blockchain: "80001",
-  //     //   address: "0x6FF32C81600Ec625c68b0D687ba3C2681eD43867",
-  //     //   contract_type: "dropper-v0.2.0",
-  //     //   offset: 0,
-  //     //   limit: 10,
-  //     // },
-  //   }).then((res) => res.data);
-  // };
+  const [requestsFound, setRequestsFound] = useState([]);
 
   const [searchResult, setSearchResult] = useState<{
     result?: string | undefined;
@@ -60,12 +50,29 @@ const DropperV2ClaimsView = ({
       url: `https://engineapi.moonstream.to/metatx/requests`,
       params: { contract_address: address, caller: searchAddress },
     })
-      .then((res: { data: { address: string; raw_amount: string } }) => {
+      .then((res: any) => {
         console.log("qq", res.data);
-        if (!res.data?.address) {
+        if (!res.data?.length) {
           throw new Error("Not found");
         }
-        setSearchResult({ result: `Amount: ${res.data.raw_amount}`, isSearching: false });
+        const amount = res.data.reduce((acc: number, req: any) => acc + req.parameters.amount, 0);
+        setSearchResult({
+          result: `${amount} token in ${res.data.length} request${
+            res.data.length > 1 ? "s" : ""
+          } found`,
+          isSearching: false,
+        });
+        setRequestsFound(
+          res.data.map((r: any) => {
+            const expiresAtDate = new Date(r.expires_at);
+            const expiresAt = `${expiresAtDate.toLocaleDateString()} ${expiresAtDate.toLocaleTimeString()}`;
+            return {
+              amount: r.parameters.amount,
+              expiresAt,
+              blockDeadline: r.parameters.blockDeadline,
+            };
+          }),
+        );
       })
       .catch((e: responseWithDetails) => {
         const result =
@@ -86,34 +93,6 @@ const DropperV2ClaimsView = ({
       toast("invalid address", "error");
     }
   };
-
-  // const contractsQuery = useQuery(["metatxContracts"], getContracts, {
-  //   onSuccess: (data) => {
-  //     console.log(data);
-  //   },
-  //   onError: (e) => {
-  //     console.log(e);
-  //   },
-  // });
-
-  // const registerContract = () => {
-  //   return http({
-  //     method: "POST",
-  //     url: "https://engineapi.moonstream.to/metatx/contracts",
-  //     data: {
-  //       blockchain: "mumbai",
-  //       address: "0x6FF32C81600Ec625c68b0D687ba3C2681eD43869",
-  //       contract_type: "dropper-v0.2.0",
-  //       title: "Test",
-  //     },
-  //   });
-  // };
-
-  // const addContract = useMutation(registerContract, {
-  //   onSuccess: () => {
-  //     contractsQuery.refetch();
-  //   },
-  // });
 
   const createRequests = () => {
     const parameters = {
@@ -150,37 +129,83 @@ const DropperV2ClaimsView = ({
   }
 
   return (
-    <Flex direction="column" gap="10px" p={5} borderRadius="10px" bg="#232323" mt="20px">
+    <Flex direction="column" gap="20px" p={5} borderRadius="10px" bg="#232323" mt="20px">
+      <Text variant="title3">Claimants</Text>
       <Flex justifyContent="start" alignItems="center">
         <Input
           value={searchAddress}
-          onChange={(e) => setSearchAddress(e.target.value)}
+          onChange={(e) => {
+            setSearchAddress(e.target.value);
+            setSearchResult({ result: undefined, isSearching: searchResult.isSearching });
+            setRequestsFound([]);
+          }}
           placeholder="search for address"
           borderRadius="10px"
           p="8px 15px"
+          spellCheck="false"
         />
-
-        <IconButton
-          icon={<SmallCloseIcon />}
-          _hover={{ color: "#ffccd4" }}
-          bg="transparent"
-          aria-label="clean"
-          onClick={() => setSearchAddress("")}
-          m="0"
-          minW="20px"
-          pl="10px"
-        />
-        <IconButton
-          _hover={{ color: "#ffccd4" }}
-          bg="transparent"
-          aria-label="search"
-          icon={<SearchIcon />}
-          minW="20px"
-          onClick={() => handleSearchClick()}
-          pl="10px"
-        />
+        {searchResult.isSearching ? (
+          <Spinner mx="18px" h="15px" w="15px" />
+        ) : (
+          <>
+            <IconButton
+              icon={<SmallCloseIcon />}
+              _hover={{ color: "#ffccd4" }}
+              bg="transparent"
+              aria-label="clean"
+              onClick={() => setSearchAddress("")}
+              m="0"
+              minW="20px"
+              pl="10px"
+            />
+            <IconButton
+              _hover={{ color: "#ffccd4" }}
+              bg="transparent"
+              aria-label="search"
+              icon={<SearchIcon />}
+              minW="20px"
+              onClick={() => handleSearchClick()}
+              pl="10px"
+            />
+          </>
+        )}
       </Flex>
-      <Button onClick={() => addClaimant.mutate()}>Add</Button>
+      {searchResult.result && <Text fontSize="14px">{searchResult.result}</Text>}
+      {requestsFound.length > 0 && (
+        <Grid templateColumns="auto auto auto" mt="-15px">
+          <GridItem textAlign="center" px="10px">
+            amount
+          </GridItem>
+          <GridItem textAlign="center" px="10px">
+            blockDeadline
+          </GridItem>
+          <GridItem textAlign="center" px="10px" mb="10px">
+            expires
+          </GridItem>
+          {requestsFound.map((req: any, idx: number) => (
+            <React.Fragment key={idx}>
+              <GridItem textAlign="center" fontFamily="monospace">
+                {req.amount}
+              </GridItem>
+              <GridItem textAlign="center" fontFamily="monospace">
+                {req.blockDeadline}
+              </GridItem>
+              <GridItem textAlign="center" fontFamily="monospace">
+                {req.expiresAt}
+              </GridItem>
+            </React.Fragment>
+          ))}
+        </Grid>
+      )}
+
+      <Button
+        placeSelf="end"
+        onClick={() => addClaimant.mutate()}
+        variant="transparent"
+        border="1px solid white"
+      >
+        Add
+      </Button>
     </Flex>
   );
 };
