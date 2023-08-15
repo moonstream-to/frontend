09f7e20be3187d5ef071fe6d551ee1b9eb9c3100
Anton Mushnin <anton.mushnin@gmail.com>
2023-08-15 09:19:34 +0300

    changing component name

--- portal/src/components/abiView/ABIViewRightPanel.tsx
+++ portal/src/components/abiView/ABIViewRightPanel.tsx
@@ -3,7 +3,7 @@ import { Flex, Input, InputGroup, InputRightElement, Text, useDisclosure } from 
 import React, { useEffect, useMemo, useRef, useState } from "react";
 import { AiOutlineClose } from "react-icons/ai";
 import Web3 from "web3";
-import CallABIFunction from "./ABIFunctionModal";
+import ABIFunctionModal from "./ABIFunctionModal";
 
 export const colorScheme = {
   name: "#7587a6",
@@ -147,7 +147,7 @@ const ABIViewRightPanel = ({
 
   return (
     <Flex bg="#282a36" w="100%" minH="100%" direction="column" overflowY="auto" flex="2">
-      <CallABIFunction
+      <ABIFunctionModal
         isOpen={isOpen}
         onClose={onClose}
         name={fnToCall?.name}
