dd1ce6de2f1a4e1b7ee2d00e4ce8f2ef2b5e2da3
Anton Mushnin <anton.mushnin@gmail.com>
2023-10-26 12:56:34 +0300

    useUpdateEntity

--- portal/src/hooks/useJournal.ts
+++ portal/src/hooks/useJournal.ts
@@ -1,25 +1,18 @@
-import { QueryFunctionContext, useMutation, useQuery, useQueryClient } from "react-query";
+import {
+  QueryClient,
+  QueryFunctionContext,
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from "react-query";
 import http from "../utils/httpMoonstream";
 import { MOONSTREAM_API_URL } from "../constants";
 import useUser from "../contexts/UserContext";
-import {
-  Entities,
-  Journal,
-  JournalInput,
-  JournalQueryData,
-  Journals,
-  JournalsResponseData,
-} from "../types";
+import { Entities, Entity, Journal, JournalInput, Journals } from "../types";
 
 const DEFAULT_JOURNAL_NAME = "Web3Addresses";
 
-interface CreateJournalResponse {
-  data: {
-    journals: Journal;
-  };
-}
-
-interface EntityOperationInput {
+interface EntityCreationInput {
   address: string;
   title: string;
   blockchain: string;
@@ -28,15 +21,13 @@ interface EntityOperationInput {
   journalName?: string;
 }
 
-interface EntityUpdateInput extends EntityOperationInput {
-  id: string; // "id" is mandatory here
-}
+type EntityUpdateInput = Partial<EntityCreationInput> & { entity: Entity };
 
 const fetchJournals = async (): Promise<Journals> => {
   const res = (await http({
     method: "GET",
     url: `${MOONSTREAM_API_URL}/journals/`,
-  })) as { data: JournalsResponseData };
+  })) as { data: { journals: Journals } };
   return res.data.journals;
 };
 
@@ -54,7 +45,7 @@ const getJournalByName = async (journalName: string): Promise<Journal | undefine
       method: "POST",
       url: "https://api.moonstream.to/journals/",
       data: { name: journalName },
-    }).then((res: CreateJournalResponse) => {
+    }).then((res: { data: { journals: Journal } }) => {
       const queryClient = useQueryClient();
       queryClient.invalidateQueries("journals");
       return res.data.journals;
@@ -63,12 +54,27 @@ const getJournalByName = async (journalName: string): Promise<Journal | undefine
   return targetJournal;
 };
 
+const getEntityId = (entity: Entity) => entity.id ?? entity.entity_url?.split("/").slice(-1)[0];
+
+export const getEntityByAddress = async (address: string) => {
+  const targetJournal = await getJournalByName(DEFAULT_JOURNAL_NAME);
+  if (!targetJournal) {
+    throw new Error("Can't create journal");
+  }
+  const journalId = targetJournal.id;
+  return http({
+    method: "GET",
+    url: `${MOONSTREAM_API_URL}/journals/${journalId}/search/?representation=entity&q=tag:address:${address}`,
+    params: { limit: 1, offset: 0 },
+  }).then((res) => res.data.results[0]);
+};
+
 //TODO comment
 const fetchJournal = async (
   context: QueryFunctionContext<
     ["journalById" | "journalByName", string, string[], number, number, string]
   >,
-): Promise<{ entities: Entities; id: string }> => {
+): Promise<{ entities: Entities; totalLength: number }> => {
   const [type, value, tags, limit, offset] = context.queryKey;
 
   let journalId: string | undefined = undefined;
@@ -84,12 +90,20 @@ const fetchJournal = async (
   }
 
   const tagsQuery = tags.map((tag) => `tag:${tag}`).join("&");
-  const res = await http({
+
+  console.log(tagsQuery);
+  const res = (await http({
     method: "GET",
-    url: `${MOONSTREAM_API_URL}/journals/${journalId}/search/?representation=entity&q=${tagsQuery}`, //TODO Limit, offset
-    params: { limit, offset },
-  });
-  return { entities: res.data.results, id: journalId };
+    url: `${MOONSTREAM_API_URL}/journals/${journalId}/search/?representation=entity`,
+    params: { limit, offset, q: tagsQuery },
+  })) as { data: { results: Entity[]; total_results: number } };
+  console.log(res);
+  return {
+    entities: res.data.results.map((e: Entity) => {
+      return { ...e, id: getEntityId(e) };
+    }),
+    totalLength: res.data.total_results,
+  };
 };
 
 export const useJournal = ({
@@ -97,11 +111,11 @@ export const useJournal = ({
   name = DEFAULT_JOURNAL_NAME,
   tags,
   limit = 10,
-  offset = 10,
+  offset = 0,
 }: JournalInput) => {
   const { user } = useUser();
   const type = id ? "journalById" : "journalByName";
-  const queryKey = [type, id ?? name, tags, limit, offset, user] as [
+  const queryKey = [type, id ?? name, tags, limit, offset, user.name] as [
     "journalById" | "journalByName",
     string,
     string[],
@@ -110,41 +124,67 @@ export const useJournal = ({
     string,
   ];
   return useQuery(queryKey, fetchJournal, {
-    enabled: !!user,
+    enabled: !!user && tags.length > 0,
     staleTime: 120000,
     keepPreviousData: true,
   });
 };
 
-export const useAddEntity = () => {
+const handleOnSuccess = (
+  newEntity: Entity,
+  variables: EntityCreationInput | EntityUpdateInput,
+  queryClient: QueryClient,
+) => {
+  queryClient.invalidateQueries("journalByName");
+};
+
+export const useUpdateEntity = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: async (input: EntityUpdateInput) => {
+      const { address, title, blockchain, secondaryFields, entity, tags } = input;
+      const entityId = getEntityId(entity);
+      console.log({ address, title, blockchain, ...secondaryFields, entityId });
+      const response = await http({
+        method: "PUT",
+        url: `https://api.moonstream.to/journals/${entity.journal_id}/entities/${entityId}`,
+        data: { address, title, blockchain, ...secondaryFields },
+      });
+      console.log(response);
+      await http({
+        method: "PUT",
+        url: `https://api.moonstream.to/journals/${entity.journal_id}/entries/${entityId}/tags`,
+        data: { tags },
+      });
+      return response.data;
+    },
+    onSuccess: (newEntity, variables) => handleOnSuccess(newEntity, variables, queryClient),
+  });
+};
+
+export const useCreateEntity = () => {
   const queryClient = useQueryClient();
 
   return useMutation({
-    mutationFn: async ({
-      address,
-      title,
-      blockchain,
-      tags,
-      secondaryFields,
-      journalName = DEFAULT_JOURNAL_NAME,
-    }: {
-      address: string;
-      title: string;
-      blockchain: string;
-      tags: string[];
-      secondaryFields?: Record<string, unknown>;
-      journalName?: string;
-    }) => {
-      const allJournals = await fetchJournals();
-      const targetJournal = allJournals.find((journal) => journal.name === journalName);
+    mutationFn: async (input: EntityCreationInput) => {
+      const {
+        journalName = DEFAULT_JOURNAL_NAME,
+        address,
+        title,
+        blockchain,
+        secondaryFields,
+        tags,
+      } = input;
+      const targetJournal = await getJournalByName(journalName);
       if (!targetJournal?.id) {
-        throw new Error(`Error: Can't find journal - ${journalName}`);
+        throw new Error(`Error: Can't find or create journal - ${journalName}`);
       }
       const response = await http({
         method: "POST",
         url: `https://api.moonstream.to/journals/${targetJournal.id}/entities`,
         data: { address, title, blockchain, ...secondaryFields },
       });
+      console.log(response);
       await http({
         method: "POST",
         url: `https://api.moonstream.to/journals/${targetJournal.id}/entries/${response.data.id}/tags`,
@@ -152,20 +192,7 @@ export const useAddEntity = () => {
       });
       return response.data;
     },
-    onSuccess: (newEntity, variables) => {
-      queryClient.invalidateQueries("journalByName");
-      queryClient.invalidateQueries("journalById");
-
-      queryClient.setQueryData<JournalQueryData>(["journalByName", variables.tags], (oldData) => {
-        if (!oldData) {
-          return { entities: [newEntity], journalId: newEntity.journalId };
-        }
-        return {
-          ...oldData,
-          entities: [...oldData.entities, newEntity], //TODO .entities have other structure
-        };
-      });
-    },
+    onSuccess: (newEntity, variables) => handleOnSuccess(newEntity, variables, queryClient),
   });
 };
 
@@ -173,32 +200,17 @@ export const useDeleteEntity = () => {
   const queryClient = useQueryClient();
 
   return useMutation({
-    mutationFn: async ({ journalName, entityId }: { entityId: string; journalName: string }) => {
-      const allJournals = await fetchJournals();
-      const targetJournal = allJournals.find((journal) => journal.name === journalName);
-      if (!targetJournal?.id) {
-        throw new Error(`Error: Can't find journal - ${journalName}`);
-      }
+    mutationFn: async ({ entity }: { entity: Entity }) => {
       const response = await http({
         method: "DELETE",
-        url: `https://api.moonstream.to/journals/${targetJournal.id}/entities/${entityId}`,
+        url: `https://api.moonstream.to/journals/${entity.journal_id}/entities/${getEntityId(
+          entity,
+        )}`,
       });
       return response.data;
     },
-    onSuccess: (newEntity, variables) => {
+    onSuccess: () => {
       queryClient.invalidateQueries("journalByName");
-      queryClient.setQueryData<JournalQueryData>(
-        ["journalByName", variables.journalName],
-        (oldData) => {
-          if (!oldData) {
-            return { entities: [], journalId: variables.journalName };
-          }
-          return {
-            ...oldData,
-            entities: [...oldData.entities.filter((e) => e.id !== variables.entityId)],
-          };
-        },
-      );
     },
   });
 };
