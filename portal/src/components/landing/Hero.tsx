059a21a02bed38eb6a2969cb0a02e498d34b95e5
Anton Mushnin <anton.mushnin@gmail.com>
2023-12-21 12:38:42 +0300

    hero

---
+++ portal/src/components/landing/Hero.tsx
@@ -0,0 +1,32 @@
+import styles from "./Hero.module.css";
+import { Flex, Image, Text, useMediaQuery } from "@chakra-ui/react";
+import { AWS_STATIC_ASSETS_PATH } from "../../constants";
+
+const Hero = () => {
+  const [is768view] = useMediaQuery(["(min-width: 768px) and (max-width: 1023px)"]);
+  return (
+    <Flex className={styles.container}>
+      <Flex className={styles.content} w={"100%"}>
+        <Text className={styles.title}>
+          Leaderboards for
+          <br /> your web3 game
+        </Text>
+        <Text className={styles.text}>
+          Automate rewarding players for their on-chain and off-chain activity.
+        </Text>
+        <button className={styles.button}>Request Demo</button>
+      </Flex>
+      <Image
+        alt={""}
+        className={styles.image}
+        src={
+          is768view
+            ? `${AWS_STATIC_ASSETS_PATH}/landing/hero-image-v.png`
+            : `${AWS_STATIC_ASSETS_PATH}/landing/hero-image-h.png`
+        }
+      />
+    </Flex>
+  );
+};
+
+export default Hero;
