c9cb04f9053d4e1d18f6f982adb73867f831bb20
Anton Mushnin <anton.mushnin@gmail.com>
2024-01-09 13:34:43 +0300

    upcoming integrations

--- portal/src/components/landing/UpcomingIntegrations.tsx
+++ portal/src/components/landing/UpcomingIntegrations.tsx
@@ -1,4 +1,4 @@
-import { Button, Flex, Image, Link, Text } from "@chakra-ui/react";
+import { Flex, Image, Link, Text } from "@chakra-ui/react";
 import { AWS_STATIC_ASSETS_PATH, DISCORD_LINK } from "../../constants";
 
 const assets = {
@@ -38,7 +38,7 @@ const UpcomingIntegrations = ({ ...props }) => {
       borderRadius="40px"
       border="1px solid white"
       p={{ base: "30px 30px", sm: "20px 20px" }}
-      gap={{ base: "30px", sm: "20px" }}
+      gap={{ base: "20px", sm: "20px" }}
       justifyContent="center"
       textAlign="center"
       bg={"#232323"}
@@ -48,29 +48,31 @@ const UpcomingIntegrations = ({ ...props }) => {
       <Text fontSize={{ base: "18px", sm: "24px" }} fontWeight="700">
         Looking for another EVM blockchain integration?
       </Text>
-      <Text fontSize={{ base: "14px", sm: "16px" }} mt="-10px">
-        <Link isExternal href={DISCORD_LINK} textDecoration="underline">
-          Contact us
-        </Link>
-        , we can add it in a couple of days.
-      </Text>
-      <Flex
-        wrap="wrap"
-        justifyContent="center"
-        columnGap={{ base: "20px", sm: "40px" }}
-        rowGap={"20px"}
-        // h="61px"
-        alignItems="center"
-      >
-        {chains.map((chain) => (
-          <Link href={chain.href} isExternal key={chain.name}>
-            <Image
-              src={assets[chain.name as keyof typeof assets]}
-              alt={chain.name}
-              h={{ base: `${chain.h}px`, sm: `${chain.h * 1.1}px` }}
-            />
+      <Flex direction={{ base: "column-reverse", sm: "column" }} gap={"20px"}>
+        <Text fontSize={{ base: "14px", sm: "16px" }} fontFamily={'"Inter", sans-serif'}>
+          <Link isExternal href={DISCORD_LINK} textDecoration="underline">
+            Contact us
           </Link>
-        ))}
+          , we can add it in a couple of days.
+        </Text>
+
+        <Flex
+          wrap="wrap"
+          justifyContent="center"
+          columnGap={{ base: "20px", sm: "40px" }}
+          rowGap={"20px"}
+          alignItems="center"
+        >
+          {chains.map((chain) => (
+            <Link href={chain.href} isExternal key={chain.name}>
+              <Image
+                src={assets[chain.name as keyof typeof assets]}
+                alt={chain.name}
+                h={{ base: `${chain.h}px`, sm: `${chain.h * 1.1}px` }}
+              />
+            </Link>
+          ))}
+        </Flex>
       </Flex>
     </Flex>
   );
