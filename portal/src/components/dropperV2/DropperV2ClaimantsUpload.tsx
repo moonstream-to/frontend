07b1b71031d0621054e910f05f363cf4d935721f
Anton Mushnin <anton.mushnin@gmail.com>
2024-02-05 18:10:16 +0300

    handling waggle and metatx errors

--- portal/src/components/dropperV2/DropperV2ClaimantsUpload.tsx
+++ portal/src/components/dropperV2/DropperV2ClaimantsUpload.tsx
@@ -76,7 +76,6 @@ const DropperV2ClaimantsUpload = ({
 
     const signers = await Promise.allSettled(requests)
       .then((results) => {
-        console.log("Promise.allSettled then", results);
         return results
           .filter(
             (r): r is PromiseFulfilledResult<{ subdomain: string; address: string }[]> =>
@@ -87,7 +86,6 @@ const DropperV2ClaimantsUpload = ({
       .catch((error) => {
         console.error("Promise.allSettled catch", error);
       });
-    console.log(signers);
     if (!signers) {
       return [];
     }
@@ -106,7 +104,6 @@ const DropperV2ClaimantsUpload = ({
           .encodeABI(),
       };
     });
-    console.log(multicallRequests);
     const multicallResults = await multicallContract.methods
       .tryAggregate(false, multicallRequests)
       .call();
@@ -127,9 +124,7 @@ const DropperV2ClaimantsUpload = ({
     serverName: string;
     signingAccount: string;
   }) => {
-    const ttl = prompt("TTL (days):");
-    // const serverName = "fullcount";
-    // const serverSigningAccount = "0x790CCaB1c3d308ceA7689C9A720f220c6C44eAD7";
+    const ttl = prompt("TTL (days):", "30");
 
     const url = `https://${serverName}.waggle.moonstream.org/signers/${signingAccount}/dropper/sign`;
     return http({
@@ -158,6 +153,88 @@ const DropperV2ClaimantsUpload = ({
     });
   };
 
+  interface ClaimRequest {
+    requestID: string;
+    dropId: string;
+    claimant: string;
+    blockDeadline: string;
+    amount: string;
+    signer?: string;
+    signature?: string;
+  }
+
+  const checkDuplicatedRequestIDs = (content: ClaimRequest[]) => {
+    const requestIDMap: Record<string, number[]> = {};
+    content.forEach((obj, index) => {
+      if (!requestIDMap[obj.requestID]) {
+        requestIDMap[obj.requestID] = [];
+      }
+      requestIDMap[obj.requestID].push(index + 1);
+    });
+
+    let message = "";
+
+    for (const requestID in requestIDMap) {
+      if (requestIDMap[requestID].length > 1) {
+        message += `requestID ${requestID} is in the rows ${requestIDMap[requestID].join(", ")}; `;
+      }
+    }
+    return message;
+  };
+
+  const checkRequestIDs = (content: ClaimRequest[]) => {
+    const wrongRequestIDs = content
+      .filter((r) => !web3.utils.isBN(r.requestID))
+      .map((r) => r.requestID);
+    return wrongRequestIDs.length > 0 ? `Invalid requestIDS: ${wrongRequestIDs.join(", ")}` : "";
+  };
+  const checkBlockDeadlines = (content: ClaimRequest[]) => {
+    const wrongRequestIDs = content
+      .filter((r) => !web3.utils.isBN(r.blockDeadline))
+      .map((r) => r.requestID);
+    return wrongRequestIDs.length > 0
+      ? `Invalid blockdeadlines: ${wrongRequestIDs.join(", ")}`
+      : "";
+  };
+
+  const checkSigners = (content: ClaimRequest[]) => {
+    const missedSigners = content.filter((r) => !r.signer).map((r) => r.requestID);
+    return missedSigners.length > 0 ? `No signer for requests: ${missedSigners.join(", ")}` : "";
+  };
+
+  const checkSignatures = (content: ClaimRequest[]) => {
+    const missedSignatures = content.filter((r) => !r.signer).map((r) => r.requestID);
+    return missedSignatures.length > 0
+      ? `No signature for requests: ${missedSignatures.join(", ")}`
+      : "";
+  };
+
+  const checkContent = ({ content, isSigned }: { content: ClaimRequest[]; isSigned: boolean }) => {
+    const duplicatedIDs = checkDuplicatedRequestIDs(content);
+    const invalidSigners = isSigned ? checkSigners(content) : "";
+    const invalidSignatures = isSigned ? checkSignatures(content) : "";
+    const invalidIDs = checkRequestIDs(content);
+    const invalidBlockdeadlines = checkBlockDeadlines(content);
+
+    const criticalErrors =
+      duplicatedIDs +
+      "\n" +
+      invalidSigners +
+      "\n" +
+      invalidSignatures +
+      "\n" +
+      invalidIDs +
+      "\n" +
+      invalidBlockdeadlines;
+    if (criticalErrors.split("\n").length !== 0) {
+      toast(criticalErrors, "error", 5000);
+    }
+
+    if (criticalErrors) {
+      return false;
+    }
+  };
+
   const onDrop = (file: any) => {
     if (!file.length) {
       return;
@@ -173,6 +250,9 @@ const DropperV2ClaimantsUpload = ({
             let response;
             if (selectedSignerAccount) {
               //SigningServer request
+              // if (!checkContent({ content, isSigned: false })) {
+              //   return;
+              // }
               const requests = content.map((item: any) => {
                 const { requestID, dropId, claimant, blockDeadline, amount } = item;
                 return {
@@ -183,15 +263,34 @@ const DropperV2ClaimantsUpload = ({
                   amount,
                 };
               });
-              response = await createSignerRequests({
-                requests,
-                serverName: selectedSignerAccount.subdomain,
-                signingAccount: selectedSignerAccount.address,
-              });
-              toast(`${response.data.requests.length} requests signed`, "success");
-              console.log(response);
+              try {
+                response = await createSignerRequests({
+                  requests,
+                  serverName: selectedSignerAccount.subdomain,
+                  signingAccount: selectedSignerAccount.address,
+                });
+                if (!response.data.metatx_registered) {
+                  toast(
+                    `FAIL - Signed ${response.data.requests.length} requests, but registering in metatx failed`,
+                    "error",
+                  );
+                } else {
+                  toast(`${response.data.requests.length} requests signed`, "success");
+                }
+              } catch (e: any) {
+                console.log(e);
+                toast(
+                  `Upload failed - ${e.response.data ?? ""} - ${
+                    e.message ?? "Error creating request"
+                  }`,
+                  "error",
+                );
+              }
             } else {
               //MetaTx request
+              // if (!checkContent({ content, isSigned: true })) {
+              //   return;
+              // }
               const specifications = content.map((item: any) => {
                 const { dropId, requestID, caller, blockDeadline, amount, signature, signer } =
                   item;
@@ -202,17 +301,29 @@ const DropperV2ClaimantsUpload = ({
                   parameters: { dropId, blockDeadline, amount, signature, signer },
                 };
               });
-              response = await createRequests({ specifications });
-              if (response.status === 200) {
-                toast(`Successfully added ${response.data} requests`, "success");
+              try {
+                response = await createRequests({ specifications });
+                if (response.status === 200) {
+                  toast(`Successfully added ${response.data} requests`, "success");
+                }
+              } catch (e: any) {
+                console.log(e);
+                if (Array.isArray(e.response.data.detail)) {
+                  const errors = e.response.data.detail
+                    .map((error: any) => `${error.loc[3]} - ${error.msg}`)
+                    .join("\n");
+                  toast(`Upload failed - \n${errors ?? "Error creating request"}`, "error", 7000);
+                } else {
+                  toast(
+                    `Upload failed - \n${e.response.data.detail ?? "Error creating request"}`,
+                    "error",
+                    7000,
+                  );
+                }
               }
             }
-
-            if (response.status === 200) {
-              console.log(response);
-              // toast(`Successfully added ${response.data} requests`, "success");
-            }
           } catch (e: any) {
+            console.log(e);
             toast(`Upload failed - ${e.message ?? "Error creating request"}`, "error");
           }
           setIsUploading(false);
