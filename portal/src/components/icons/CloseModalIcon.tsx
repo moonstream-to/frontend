c90418c210e44c1c69508a27e8e419f35a7becdf
Anton Mushnin <anton.mushnin@gmail.com>
2024-02-06 12:33:45 +0300

    adding icons

---
+++ portal/src/components/icons/CloseModalIcon.tsx
@@ -0,0 +1,18 @@
+// In /components/icons/MyIcon.tsx
+import React from "react";
+
+const CloseModalIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    {...props}
+  >
+    <path d="M6 6L19 19" stroke="#EE8686" strokeWidth="1.5" strokeLinecap="round" />
+    <path d="M19 6L6 19" stroke="#EE8686" strokeWidth="1.5" strokeLinecap="round" />
+  </svg>
+);
+
+export default CloseModalIcon;
---
+++ portal/src/components/icons/ErrorOctagonIcon.tsx
@@ -0,0 +1,30 @@
+// In /components/icons/MyIcon.tsx
+import React from "react";
+
+const ErrorOctagonIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+    <path
+      d="M7.86 2H16.14L22 7.86V16.14L16.14 22H7.86L2 16.14V7.86L7.86 2Z"
+      stroke="#EE8686"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M15 9L9 15"
+      stroke="#EE8686"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M9 9L15 15"
+      stroke="#EE8686"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export default ErrorOctagonIcon;
