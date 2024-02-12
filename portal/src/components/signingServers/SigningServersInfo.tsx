e03fa1326aad87159dac03cdb6a2ce32d5f1de9a
Anton Mushnin <anton.mushnin@gmail.com>
2024-02-12 22:00:55 +0300

    new signing servers info

--- portal/src/components/signingServers/SigningServersInfo.tsx
+++ portal/src/components/signingServers/SigningServersInfo.tsx
@@ -2,24 +2,69 @@ import { useQuery } from "react-query";
 import axios from "axios";
 import { Flex, Spinner, Text } from "@chakra-ui/react";
 import PoolDetailsRow from "../PoolDetailsRow";
-import { formatDuration, timestampToHumanDate } from "../../utils/timeFormat";
 import React from "react";
+import useUser from "../../contexts/UserContext";
 
 const SigningServersInfo = () => {
-  const signingServers = useQuery(["nodeBalancerAccess"], () => {
+  const { user } = useUser();
+  const signingServer = useQuery(["signing_server", user], async () => {
     const token = localStorage.getItem("MOONSTREAM_ACCESS_TOKEN");
     const authorization = token ? { Authorization: `Bearer ${token}` } : {};
+    const moonstreamApplicationId = process.env.NEXT_PUBLIC_MOONSTREAM_APPLICATION_ID;
 
-    return axios
-      .get("https://fullcount.waggle.moonstream.org/signers/", {
-        headers: {
-          "Content-Type": "application/json",
-          ...authorization,
+    const subdomains = await axios
+      .get(
+        `https://auth.bugout.dev/resources?application_id=${moonstreamApplicationId}&type=waggle-access`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            ...authorization,
+          },
         },
-      })
+      )
       .then((res) => {
-        return res.data;
+        return res.data.resources
+          .filter((r: any) => r.resource_data.type === "waggle-access")
+          .map((r: any) => r.resource_data.customer_name);
       });
+
+    const requests = subdomains.map((subdomain: string) => {
+      return axios
+        .get(`https://${subdomain}.waggle.moonstream.org/signers/`, {
+          headers: {
+            "Content-Type": "application/json",
+            ...authorization,
+          },
+        })
+        .then((res) => {
+          return res.data.signers.map((s: string) => {
+            return { subdomain: subdomain, address: s };
+          });
+        });
+    });
+
+    const signers = await Promise.allSettled(requests)
+      .then((results) => {
+        return results
+          .filter(
+            (res): res is PromiseFulfilledResult<{ subdomain: string; address: string }[]> =>
+              res.status === "fulfilled",
+          )
+          .flatMap((result) => result.value);
+      })
+      .catch((error) => {
+        console.error("Promise.allSettled catch", error);
+      });
+    const unavailableServers = subdomains.filter(
+      (subdomain: string) =>
+        !signers ||
+        !signers.some((signer: { subdomain: string }) => signer.subdomain === subdomain),
+    );
+    if (!signers) {
+      return { signers: [], unavailableServers };
+    }
+
+    return { signers, unavailableServers };
   });
 
   return (
@@ -38,16 +83,19 @@ const SigningServersInfo = () => {
       <Text placeSelf="center" fontSize="22px" fontWeight="700" mb="20px">
         Signing servers
       </Text>
-      {signingServers.isLoading && <Spinner />}
+      {signingServer.isLoading && <Spinner />}
 
-      {signingServers.data && (
+      {signingServer.data && (
         <Flex direction={"column"} gap={"20px"} w={"100%"}>
-          {signingServers.data.signers.map((item: string, idx: number) => (
-            <Flex direction={"column"} gap={"10px"} key={idx}>
-              <PoolDetailsRow displayFull value={item} type={"Account"} />
-              <PoolDetailsRow displayFull value={"OK"} type={"Status"} />
-            </Flex>
-          ))}
+          {signingServer.data.signers.map(
+            (item: { subdomain: string; address: string }, idx: number) => (
+              <Flex direction={"column"} gap={"10px"} key={idx}>
+                <PoolDetailsRow displayFull value={item.subdomain} type={"Server"} />
+                <PoolDetailsRow displayFull value={item.address} type={"Account"} />
+                <PoolDetailsRow displayFull value={"OK"} type={"Status"} />
+              </Flex>
+            ),
+          )}
         </Flex>
       )}
     </Flex>
