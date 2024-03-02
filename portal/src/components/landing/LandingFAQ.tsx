f5bc7d65941682170699814580972d7e4e14f741
Anton Mushnin <anton.mushnin@gmail.com>
2024-03-02 16:21:49 +0300

    FAQ update

--- portal/src/components/landing/LandingFAQ.tsx
+++ portal/src/components/landing/LandingFAQ.tsx
@@ -1,18 +1,10 @@
 import { Flex, Link, Text, Accordion } from "@chakra-ui/react";
 import { DISCORD_LINK } from "../../constants";
 import FAQCard from "../FAQCard";
-
+import styles from "./LandingFAQ.module.css";
 const LandingFAQ = () => {
   return (
-    <Flex
-      py={{ base: "40px", sm: "80px" }}
-      px={{ base: "7%", xl: "101px" }}
-      gap={{ base: "40px", sm: "60px" }}
-      direction="column"
-      alignItems="center"
-      w={"100%"}
-      maxW={"1440px"}
-    >
+    <Flex className={styles.container}>
       <Text fontSize={{ base: "30px", sm: "40px" }} fontWeight="700">
         FAQ
       </Text>
@@ -76,43 +68,6 @@ const LandingFAQ = () => {
           }
         />
         <FAQCard
-          heading="I’m a data scientist. Can I use Moonstream for research?"
-          panelContent={
-            <>
-              You can use{" "}
-              <Link
-                _hover={{ color: "accent.500" }}
-                href="https://github.com/bugout-dev/moonworm"
-                isExternal
-              >
-                <u>Moonworm</u>
-              </Link>
-              , our free open source tool, to build datasets of on-chain data related to market
-              activity.
-              <br />
-              <br />
-              We also have a dataset with on-chain activity from the Ethereum NFT market (April 1 to
-              September 25, 2021){" "}
-              <Link
-                _hover={{ color: "accent.500" }}
-                href="https://www.kaggle.com/datasets/simiotic/ethereum-nfts"
-                isExternal
-              >
-                <u>here</u>
-              </Link>
-              . And{" "}
-              <Link
-                _hover={{ color: "accent.500" }}
-                href="https://github.com/bugout-dev/moonstream/blob/main/datasets/nfts/papers/ethereum-nfts.pdf"
-                isExternal
-              >
-                <u>here</u>
-              </Link>{" "}
-              is our full report on it.
-            </>
-          }
-        />
-        <FAQCard
           heading="I don’t see the blockchain I need in the supported blockchains, can you add it?"
           panelContent={
             <>
@@ -153,6 +108,33 @@ const LandingFAQ = () => {
                 contact us
               </Link>{" "}
               to get the pricing information.
+            </>
+          }
+        />
+        <FAQCard
+          heading="How secure are operations run with Moonstream?"
+          panelContent={
+            <>
+              We take security very seriously. We have managed smart contracts with hundreds of
+              millions of dollars in transactions going through them. If you want to know more about
+              how we run our operations, read this article:{" "}
+              <Link
+                isExternal
+                href={"https://medium.com/@moonstream/open-tooling-hidden-operations-c2033f17b33e"}
+                textDecoration="underline"
+                _hover={{ color: "accent.500" }}
+              >
+                Open tooling. Hidden operations.
+              </Link>
+            </>
+          }
+        />
+        <FAQCard
+          heading="Can I set up a leaderboard for game server actions that are not on-chain?"
+          panelContent={
+            <>
+              Yes, our leaderboards work for both on-chain and off-chain player actions. You can
+              even combine them into one leaderboard!
             </>
           }
         />
