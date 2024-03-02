b88782e4284ceb28b784efdf3f42857a2a7e9dc9
Anton Mushnin <anton.mushnin@gmail.com>
2024-03-02 18:40:30 +0300

    gradient border

--- portal/src/components/landing/LandingFeatureCard.tsx
+++ portal/src/components/landing/LandingFeatureCard.tsx
@@ -20,28 +20,30 @@ const LandingFeatureCard = ({
   [x: string]: any;
 }) => {
   return (
-    <Flex className={styles.container} {...props}>
-      <Image src={feature.image} w="140px" h="140px" alt={feature.name} />
-      <Flex className={styles.content}>
-        <Text
-          className={isMainCard ? styles.gradientText : styles.title}
-          fontSize="24px"
-          fontWeight="700"
-          mb="10px"
-        >
-          {feature.name}
-        </Text>
-        <div
-          dangerouslySetInnerHTML={{ __html: feature.description }}
-          className={styles.description}
-        />
-        <Link
-          href={feature.href}
-          style={{ pointerEvents: disabled ? "none" : "all" }}
-          className={styles.link}
-        >
-          Learn more
-        </Link>
+    <Flex className={isMainCard ? styles.gradientContainer : styles.solidContainer} {...props}>
+      <Flex className={styles.container}>
+        <Image src={feature.image} w="140px" h="140px" alt={feature.name} />
+        <Flex className={styles.content}>
+          <Text
+            className={isMainCard ? styles.gradientText : styles.title}
+            fontSize="24px"
+            fontWeight="700"
+            mb="10px"
+          >
+            {feature.name}
+          </Text>
+          <div
+            dangerouslySetInnerHTML={{ __html: feature.description }}
+            className={styles.description}
+          />
+          <Link
+            href={feature.href}
+            style={{ pointerEvents: disabled ? "none" : "all" }}
+            className={styles.link}
+          >
+            Learn more
+          </Link>
+        </Flex>
       </Flex>
     </Flex>
   );
